1c22ec973d1b1f4ccb5db9056ca807f9ef653f78
Eric R <rasliche@gmail.com>
2019-03-13 14:26:48 -0400

    fill out store to populate initial operators on admin page and create a new operator

--- src/store.js
+++ src/store.js
@@ -1,10 +1,37 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import axios from 'axios'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
+  state: {
+    operators: []
+  },
+  getters: {
+    operators: state => {
+      return state.operators
+    }
+  },
+  mutations: {
+    setOperators: (state, operators) => {
+      state.operators = operators
+    },
+    addOperator: (state, operator) => {
+      state.operators.push(operator)
+    }
+  },
+  actions: {
+    fetchAllOperators: async ({ commit }) => {
+      const { data } = await axios.get('http://localhost:3000/api/operators')
+      commit('setOperators', data)
+    },
+    createNewOperator: async ({ commit }, payload) => {
+      console.log(payload)
+      const { data, status } = await axios.post('http://localhost:3000/api/operators', payload)
+      if (status === 201) {
+        commit('addOperator', data)
+      }
+    }
+  }
 });
