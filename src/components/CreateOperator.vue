337e93d55c71f9b21bab2747dce7755c5876791a
Eric R <rasliche@gmail.com>
2019-03-13 14:27:01 -0400

    create operator form component

---
+++ src/components/CreateOperator.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="border">
+        <form>
+            <label for="name">Operator Name</label>
+            <input type="text" id="name" v-model="name">
+            <button @click.prevent="submitOperator">Create</button>
+        </form>
+        <div>
+            <p>Operator Name: {{ name }}</p>
+        </div>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapActions } from 'vuex'
+
+export default {
+    data() {
+        return {
+            name: '',
+            region: ''
+        }
+    },
+    methods: {
+        ...mapActions([
+            'createNewOperator'
+        ]),
+        submitOperator() {
+            const newOperator = {
+                name: this.name
+            }
+            this.createNewOperator(newOperator)
+            this.name = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
