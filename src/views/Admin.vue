239884fb1bc937b796f535e4421a885e2736b20a
Eric R <rasliche@gmail.com>
2019-03-13 20:14:34 -0400

    admin dashboard skeleton

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,8 +1,30 @@
 <template>
   <div class="home">
-    <CreateOperator />
-    <button @click="fetchAllOperators">Refresh Operator List</button>
-    {{ operators }}
+    <h1>Admin Dashboard</h1>
+    <div class="flex flex-wrap">
+      <div class="w-1/2">
+        <h3>Operators</h3>
+        <CreateOperator />
+        <p v-for="operator in operators" :key="operator._id">
+          {{ operator.name }}
+        </p>
+      </div>
+      <div class="w-1/2">
+        <h3>Staff</h3>
+      </div>
+      <div class="w-1/2">
+        <h3>Lessons</h3>
+      </div>
+      <div class="w-1/2">
+        <h3>Quizzes</h3>
+      </div>
+      <div class="w-1/2">
+        <h3>Blog Posts</h3>
+      </div>
+      <div class="w-1/2">
+        <h3>Events</h3>
+      </div>
+    </div>
   </div>
 </template>
 
