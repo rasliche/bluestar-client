d473e9e243a55f774f300813bac9b0bd4b02414d
Eric R <rasliche@gmail.com>
2019-03-15 08:53:13 -0400

    components to view an operator and a list of operators

---
+++ src/views/Operators.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <h1>All Shops</h1>
+        {{ operators }}
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapActions } from 'vuex'
+
+export default {
+    methods: {
+        ...mapActions([
+            'fetchAllOperators'
+        ])
+    },
+    mounted() {
+        // this.fetchAllOperators()
+    },
+    computed: {
+        operators() {
+            return this.$store.getters.operators
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
