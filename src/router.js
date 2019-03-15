66b2576250b5c59f2972f5ce3dc64ce2cd4d3113
Eric R <rasliche@gmail.com>
2019-03-15 08:52:31 -0400

    operators route

--- src/router.js
+++ src/router.js
@@ -25,6 +25,15 @@ export default new Router({
         import(/* webpackChunkName: "about" */ "./views/About.vue")
     },
     {
+      path: "/operators",
+      name: "operators",
+      // route level code-splitting
+      // this generates a separate chunk (operators.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "operators" */ "./views/Operators.vue")
+    },
+    {
       path: "/news",
       name: "news",
       // route level code-splitting
