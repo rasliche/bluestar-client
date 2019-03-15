d93d25f829170dc9f44cbccee389e60f683cf842
Eric R <rasliche@gmail.com>
2019-03-15 08:52:45 -0400

    component to create a quiz

---
+++ src/components/CreateQuiz.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="border">
+        <form>
+            <label for="title">Quiz Title</label>
+            <input type="text" id="title" v-model="title">
+            <button @click.prevent="submitQuiz">Create</button>
+        </form>
+    </div>
+</template>
+
+<script>
+import axios from 'axios'
+import { mapActions } from 'vuex'
+
+export default {
+    data() {
+        return {
+            title: ''
+        }
+    },
+    methods: {
+        ...mapActions([
+            'createNewQuiz'
+        ]),
+        submitQuiz() {
+            const newQuiz = {
+                title: this.title,
+            }
+            this.createNewQuiz(newQuiz)
+            this.title = ''
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
