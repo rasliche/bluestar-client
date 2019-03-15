522c7049cc4bcb1ff4a8ac925b59430b9768e412
Eric R <rasliche@gmail.com>
2019-03-15 10:09:26 -0400

    new training nav

---
+++ src/components/TrainingNav.vue
@@ -0,0 +1,17 @@
+<template>
+    <nav class="flex flex-col">
+        <!-- TODO: iterate through training children -->
+        <router-link :to="{ name: 'training'}">Training Home</router-link>
+        <router-link :to="{ name: 'about-fknms'}">About FKNMS</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
