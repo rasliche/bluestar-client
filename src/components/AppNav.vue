fec46857ebdd764654445e6c17a2b63969f7c156
Eric R <rasliche@gmail.com>
2019-03-16 21:36:22 -0400

    App wide nav component

---
+++ src/components/AppNav.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="nav" class="py-4">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/news">News</router-link> |
+      <router-link to="/training">Training</router-link> |
+      <router-link to="/admin">Admin</router-link> |
+      <router-link to="/operators">Operators</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
