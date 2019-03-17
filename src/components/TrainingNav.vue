69bcf4ae8b2af79b792f8b8be656a23c8358a575
Eric R <rasliche@gmail.com>
2019-03-16 21:36:45 -0400

    add all lessons to training navigation

--- src/components/TrainingNav.vue
+++ src/components/TrainingNav.vue
@@ -2,7 +2,16 @@
     <nav class="flex flex-col">
         <!-- TODO: iterate through training children -->
         <router-link :to="{ name: 'training'}">Training Home</router-link>
+        <router-link :to="{ name: 'about-blue-star'}">About Blue Star</router-link>
         <router-link :to="{ name: 'about-fknms'}">About FKNMS</router-link>
+        <router-link :to="{ name: 'regulations'}">Regulations</router-link>
+        <router-link :to="{ name: 'cultural-resources'}">Cultural Resources</router-link>
+        <router-link :to="{ name: 'mangroves'}">Mangroves</router-link>
+        <router-link :to="{ name: 'seagrasses'}">Seagrasses</router-link>
+        <router-link :to="{ name: 'coral-reefs'}">Coral Reefs</router-link>
+        <router-link :to="{ name: 'threats-to-corals'}">Threats to Corals</router-link>
+        <router-link :to="{ name: 'reef-etiquette'}">Reef Etiquette</router-link>
+        <router-link :to="{ name: 'invasive-lionfish'}">Invasive Lionfish</router-link>
     </nav>
 </template>
 
@@ -13,5 +22,7 @@ export default {
 </script>
 
 <style>
-
+a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
