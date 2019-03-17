67fcf0ae38ef6b7c64b7a6ee38e6805adc64fd6a
Eric R <rasliche@gmail.com>
2019-03-16 21:36:56 -0400

    login to a shop

---
+++ src/components/auth/Login.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Login Component</h1>
+        <form>
+            <select name="shopChoice" id="shopChoice">
+                <option v-for="operator in operators" :value="operator" :key="operator._id">{{ operator.name }}</option>
+            </select>
+            <br>
+            <label for="shopPassword">Shop Password</label>
+            <input type="text" name="shopPassword" id="shopPassword">
+        </form>
+    </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+    computed: {
+        ...mapGetters([
+            'operators'
+        ])
+    }
+}
+</script>
+
+<style>
+
+</style>
