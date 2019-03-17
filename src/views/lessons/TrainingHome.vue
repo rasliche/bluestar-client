c468a6ecb65ced25ef9e98ac63b869a62ccec2c9
Eric R <rasliche@gmail.com>
2019-03-16 21:37:46 -0400

    training home page

--- src/views/lessons/TrainingHome.vue
+++ src/views/lessons/TrainingHome.vue
@@ -3,11 +3,17 @@
         <h1>Blue Star Training</h1>
         <p>Welcome to the Blue Star training. This training is required for your dive/snorkel shop to be recognized as a Blue Star operator.</p>
         <p>Please select your dive/snorkel shop from the drop down menu and enter your shop's password.</p>
+        <Login />
     </div>
 </template>
 
 <script>
+import Login from '@/components/auth/Login.vue'
+
 export default {
+    components: {
+        Login
+    }
 
 }
 </script>
