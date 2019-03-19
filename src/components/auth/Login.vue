f62feaf6af75859861acc5a03fc43c98d0da0239
Eric R <rasliche@gmail.com>
2019-03-19 15:18:13 -0400

    login component

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -2,26 +2,24 @@
     <div>
         <h1>Login</h1>
         <form class="flex flex-wrap">
-            <label for="userEmail" class="w-1/2 text-right pr-2 border border-red">User Email:</label>
-            <input type="text" name="userEmail" id="userEmail" class="w-1/2 text-left pl-2 border border-red">
-            <label for="shopChoice" class="w-1/2">Choose a Shop:</label>
-            <select name="shopChoice" id="shopChoice" class="w-1/2">
-                <option v-for="operator in operators" :value="operator" :key="operator._id">{{ operator.name }}</option>
-            </select>
-            <label for="shopPassword" class="w-1/2">Shop Password:</label>
-            <input type="text" name="shopPassword" id="shopPassword" class="w-1/2">
+            <label for="email" class="w-1/2 text-right pr-2 border border-red">User Email:</label>
+            <input type="text" name="email" id="email" v-model="email" class="w-1/2 text-left pl-2 border border-red">
+
+            <label for="password" class="w-1/2 text-right pr-2 border border-red">Password:</label>
+            <input type="text" name="password" id="password" v-model="password" class="w-1/2">
         </form>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import axios from 'axios'
 
 export default {
-    computed: {
-        ...mapGetters([
-            'operators'
-        ])
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
     }
 }
 </script>
