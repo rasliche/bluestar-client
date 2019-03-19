a06b0d0c72e7d9eed455eda38aeddac43c329576
Eric R <rasliche@gmail.com>
2019-03-19 15:18:03 -0400

    a users list component

---
+++ src/components/UsersList.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>List of Users</h1>
+        <p>Should accept props that dictate what fields to show.</p>
+        <ul>
+            <li v-for="user in users" :key="user._id">{{ user.name }}</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["users", "fields"]
+}
+</script>
