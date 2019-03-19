79987b336c9c6a5b580e94adec1ed74927d917c1
Eric R <rasliche@gmail.com>
2019-03-19 15:18:50 -0400

    login or register as a new user on the training landing page

--- src/views/lessons/TrainingHome.vue
+++ src/views/lessons/TrainingHome.vue
@@ -4,15 +4,18 @@
         <p>Welcome to the Blue Star training. This training is required for your dive/snorkel shop to be recognized as a Blue Star operator.</p>
         <p>Please select your dive/snorkel shop from the drop down menu and enter your shop's password.</p>
         <Login />
+        <Register />
     </div>
 </template>
 
 <script>
 import Login from '@/components/auth/Login.vue'
+import Register from '@/components/auth/Register.vue'
 
 export default {
     components: {
-        Login
+        Login,
+        Register
     }
 
 }
