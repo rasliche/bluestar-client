57017665ab1804827314d686c1633495c43e1b15
Eric R <rasliche@gmail.com>
2019-03-19 15:17:48 -0400

    first stab at styling the site navigation

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,11 +1,11 @@
 <template>
-    <div id="nav" class="py-4">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/news">News</router-link> |
-      <router-link to="/training">Training</router-link> |
-      <router-link to="/admin">Admin</router-link> |
-      <router-link to="/operators">Operators</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="nav" class="flex flex-col bg-blue-darker">
+      <router-link to="/" class="text-center py-4 text-3xl bg-blue-darkest text-blue-lightest no-underline">Blue Star</router-link>
+      <router-link to="/news" class="ml-10 mt-10 no-underline text-blue-light">News</router-link>
+      <router-link to="/training" class="ml-10 mt-3 no-underline text-blue-light">Training</router-link>
+      <router-link to="/operators" class="ml-10 mt-3 no-underline text-blue-light">Operators</router-link>
+      <router-link to="/about" class="ml-10 mt-3 no-underline text-blue-light">About</router-link>
+      <router-link to="/admin" class="ml-10 mt-3 no-underline text-blue-light">Admin</router-link>
     </div>
 </template>
 
@@ -16,5 +16,7 @@ export default {
 </script>
 
 <style>
-
+a.router-link-active {
+  @apply text-blue-lightest ml-6;
+}
 </style>
