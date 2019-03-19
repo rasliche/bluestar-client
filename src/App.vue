6b981c9ae8b83d974e4cc71c83e829da8eb774bf
Eric R <rasliche@gmail.com>
2019-03-19 15:17:09 -0400

    Set up overall app component arrangement

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app" class="px-6">
-    <AppNav />
-    <router-view />
+  <div id="app" class="flex">
+    <AppNav class="w-1/6 h-screen" />
+    <router-view class="w-5/6" />
   </div>
 </template>
 
@@ -23,25 +23,5 @@ export default {
   }
 }
 </script>
-
-
-<style>
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
 
 <style src="./styles/styles.css"></style>
