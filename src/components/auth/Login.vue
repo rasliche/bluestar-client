a11368272f9a9bff086d1ef100aab4458d30cc5b
Eric R <rasliche@gmail.com>
2019-03-19 10:28:25 -0400

    some better styling for login form

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <h1>Login Component</h1>
-        <form>
-            <select name="shopChoice" id="shopChoice">
+        <h1>Login</h1>
+        <form class="flex flex-wrap">
+            <label for="userEmail" class="w-1/2 text-right pr-2 border border-red">User Email:</label>
+            <input type="text" name="userEmail" id="userEmail" class="w-1/2 text-left pl-2 border border-red">
+            <label for="shopChoice" class="w-1/2">Choose a Shop:</label>
+            <select name="shopChoice" id="shopChoice" class="w-1/2">
                 <option v-for="operator in operators" :value="operator" :key="operator._id">{{ operator.name }}</option>
             </select>
-            <br>
-            <label for="shopPassword">Shop Password</label>
-            <input type="text" name="shopPassword" id="shopPassword">
+            <label for="shopPassword" class="w-1/2">Shop Password:</label>
+            <input type="text" name="shopPassword" id="shopPassword" class="w-1/2">
         </form>
     </div>
 </template>
@@ -23,7 +25,3 @@ export default {
     }
 }
 </script>
-
-<style>
-
-</style>
