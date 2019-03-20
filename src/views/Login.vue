162d111efe7b5743aeaf1cb0fccbd1889e44faba
Eric R <rasliche@gmail.com>
2019-03-20 13:14:18 -0400

    login page to log in and register

---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <Login />
+        <Register />
+    </div>
+</template>
+
+<script>
+import Login from '@/components/auth/Login'
+import Register from '@/components/auth/Register'
+export default {
+    components: {
+        Login,
+        Register
+    }
+}
+</script>
+
+<style>
+
+</style>
