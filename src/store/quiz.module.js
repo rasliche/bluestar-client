da7f83d7105dc03323e08ac5ae381fde446ca2c0
Eric R <rasliche@gmail.com>
2019-03-20 13:13:44 -0400

    quiz store module to work with quizzes

---
+++ src/store/quiz.module.js
@@ -0,0 +1,23 @@
+const state = {
+    quiz: 'A blue star quiz'
+}
+
+const getters = {
+
+}
+
+const mutations = {
+
+}
+
+const actions = {
+
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    mutations,
+    actions
+}
