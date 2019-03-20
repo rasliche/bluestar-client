cfbc1d7115e844c0da8a4a40d5c6e7a732af4255
Eric R <rasliche@gmail.com>
2019-03-20 13:12:59 -0400

    basic styling on login component

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="text-center">
         <h1>Login</h1>
         <form class="flex flex-wrap">
             <label for="email" class="w-1/2 text-right pr-2 border border-red">User Email:</label>
@@ -7,18 +7,30 @@
 
             <label for="password" class="w-1/2 text-right pr-2 border border-red">Password:</label>
             <input type="text" name="password" id="password" v-model="password" class="w-1/2">
+
+            <button @click.prevent="submitLogin" class="border-2 rounded border-blue">Login</button>
         </form>
     </div>
 </template>
 
 <script>
-import axios from 'axios'
+import { mapActions } from 'vuex'
 
 export default {
     data() {
         return {
             email: '',
             password: ''
+        }
+    },
+    methods: {
+        async submitLogin() {
+            const authData = { 
+                email: this.email,
+                password: this.password
+            }
+
+            this.$store.dispatch('user/login', authData)
         }
     }
 }
