0406794d2fbeeda36d0b59eca9e5249cb3eb8ed1
Eric R <rasliche@gmail.com>
2019-03-20 13:11:13 -0400

    store now uses modules for user and quiz

--- src/store.js
+++ src/store.js
@@ -1,32 +1,44 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import axios from 'axios'
+import axios from '@/services/axios-api'
+
+import user from '@/store/user.module'
+import quiz from '@/store/quiz.module'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    activeUser: null,
     operators: []
   },
+
+  modules: {
+    user,
+    quiz
+  },
+
   getters: {
     operators: state => {
       return state.operators
     }
   },
+
   mutations: {
     setOperators: (state, operators) => {
       state.operators = operators
     },
+    
     addOperator: (state, operator) => {
       state.operators.push(operator)
     }
   },
+
   actions: {
     fetchAllOperators: async ({ commit }) => {
       const { data } = await axios.get('http://localhost:3000/api/operators')
       commit('setOperators', data)
     },
+
     createNewOperator: async ({ commit }, payload) => {
       console.log(payload)
       const { data, status } = await axios.post('http://localhost:3000/api/operators', payload)
