f9cf4e014aed8f267fa7eef6a63f2fbc5c8623cf
Eric R <rasliche@gmail.com>
2019-03-20 13:13:56 -0400

    removed styling from admin view

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,28 +1,28 @@
 <template>
   <div class="home">
     <h1>Admin Dashboard</h1>
-    <div class="flex flex-wrap">
-      <div class="w-1/2">
+    <div>
+      <div>
         <h3>Operators</h3>
         <CreateOperator />
         <p v-for="operator in operators" :key="operator._id">
           {{ operator.name }}
         </p>
       </div>
-      <div class="w-1/2">
+      <div>
         <h3>Users</h3>
         <UsersList :users="users" />
       </div>
-      <div class="w-1/2">
+      <div>
         <h3>Lessons</h3>
       </div>
-      <div class="w-1/2">
+      <div>
         <h3>Quizzes</h3>
       </div>
-      <div class="w-1/2">
+      <div>
         <h3>Blog Posts</h3>
       </div>
-      <div class="w-1/2">
+      <div>
         <h3>Events</h3>
       </div>
     </div>
