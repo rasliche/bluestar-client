f8bc1d4ae4749a8cd434e1eac3441711ced0c7d5
Eric R <rasliche@gmail.com>
2019-03-22 16:05:18 -0400

    Training home page is barebones

--- src/views/lessons/TrainingHome.vue
+++ src/views/lessons/TrainingHome.vue
@@ -3,20 +3,11 @@
         <h1>Blue Star Training</h1>
         <p>Welcome to the Blue Star training. This training is required for your dive/snorkel shop to be recognized as a Blue Star operator.</p>
         <p>Please select your dive/snorkel shop from the drop down menu and enter your shop's password.</p>
-        <Login />
-        <Register />
     </div>
 </template>
 
 <script>
-import Login from '@/components/auth/Login.vue'
-import Register from '@/components/auth/Register.vue'
-
 export default {
-    components: {
-        Login,
-        Register
-    }
 
 }
 </script>
