64ea9f3794c3c1298d8e277f7f50c2a4b5bc3022
Eric R <rasliche@gmail.com>
2019-03-22 15:59:10 -0400

    show routes based on user authentication level

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,16 +1,23 @@
 <template>
-    <div class="flex flex-col bg-blue-darker">
-      <!-- <router-link to="/news" class="pl-10 mt-10 no-underline text-blue-light">News</router-link> -->
+    <div class="flex flex-col bg-blue-darker min-h-screen">
+      <router-link to="/news" class="pl-10 mt-10 no-underline text-blue-light">News</router-link>
       <router-link to="/training" class="pl-10 mt-3 no-underline text-blue-light">Training</router-link>
-      <!-- <router-link to="/operators" class="pl-10 mt-3 no-underline text-blue-light">Operators</router-link> -->
-      <!-- <router-link to="/about" class="pl-10 mt-3 no-underline text-blue-light">About</router-link> -->
-      <router-link to="/admin" class="pl-10 mt-3 no-underline text-blue-light">Admin</router-link>
+      <router-link v-if="auth" to="/operators" class="pl-10 mt-3 no-underline text-blue-light">Operators</router-link>
+      <router-link to="/about" class="pl-10 mt-3 no-underline text-blue-light">About</router-link>
+      <router-link v-if="admin" to="/admin" class="pl-10 mt-3 no-underline text-blue-light">Admin</router-link>
     </div>
 </template>
 
 <script>
-export default {
+import { mapGetters } from 'vuex'
 
+export default {
+  computed: {
+    ...mapGetters('user', {
+      auth: 'isAuthenticated',
+      admin: 'isAdmin'
+    })
+  }
 }
 </script>
 
