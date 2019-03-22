1f41862fdfd5e5d2d2ea3d75cb432dc871e2896a
Eric R <rasliche@gmail.com>
2019-03-22 15:58:14 -0400

    import store and re-enable routes and remove comments

--- src/router.js
+++ src/router.js
@@ -5,96 +5,88 @@ import Home from "./views/Home.vue";
 import AboutFKNMS from "./views/lessons/AboutFKNMS.vue"
 import TrainingHome from "./views/lessons/TrainingHome.vue"
 
+import store from './store'
+
 Vue.use(Router);
 
 export default new Router({
   mode: "history",
   base: process.env.BASE_URL,
   routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
+    { path: "/", name: "home", component: Home },
     {
       path: "/login",
       name: "login",
+      // route level code-splitting
+      // this generates a separate chunk (login.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "login" */ "./views/Login.vue")
     },
-    // {
-    //   path: "/about",
-    //   name: "about",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (about.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "about" */ "./views/About.vue")
-    // },
-    // {
-    //   path: "/operators",
-    //   name: "operators",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (operators.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "operators" */ "./views/Operators.vue")
-    // },
-    // {
-    //   path: "/operators/:slug",
-    //   name: "operator",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (operator.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "operator" */ "./views/Operator.vue")
-    // },
-    // {
-    //   path: "/news",
-    //   name: "news",
-    //   // route level code-splitting
-    //   // this generates a separate chunk (news.[hash].js) for this route
-    //   // which is lazy-loaded when the route is visited.
-    //   component: () =>
-    //     import(/* webpackChunkName: "news" */ "./views/News.vue")
-    // },
     {
-      path: "/training",
+      path: '/me',
+      name: 'me',
+      beforeEnter (to, from, next) {
+        if (store.state.user.token) {
+          next()
+        } else {
+          next('/login')
+        }
+      },
+      component: () => import(/* webpackChunkName: 'me' */ "./views/UserDashboard.vue")
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+    },
+    {
+      path: "/operators",
+      name: "operators",
+      component: () => import(/* webpackChunkName: "operators" */ "./views/Operators.vue")
+    },
+    {
+      path: "/operators/:slug",
+      name: "operator",
       // route level code-splitting
-      // this generates a separate chunk (training.[hash].js) for this route
+      // this generates a separate chunk (operator.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () =>
-        import(/* webpackChunkName: "training" */ "./views/Training.vue"),
+        import(/* webpackChunkName: "operator" */ "./views/Operator.vue")
+    },
+    {
+      path: "/news",
+      name: "news",
+      component: () => import(/* webpackChunkName: "news" */ "./views/News.vue")
+    },
+    {
+      path: "/training",
+      beforeEnter: (to, from, next) => {
+        console.log("beforeEnter route guard")
+        console.log("Alert user they are not signed in")
+        console.log(to)
+        console.log(from)
+        next()
+      },
+      component: () => import(/* webpackChunkName: "training" */ "./views/Training.vue"),
       children: [
-        {
-          path: '',
-          name: "training",
-          component: TrainingHome
-        },
-        {
-          path: 'about-fknms',
-          name: "about-fknms",
-          component: AboutFKNMS
-        },
-        {
-          path: "*",
-          name: "four-oh-four",
-          component: () => import(/* webpackChunkName: "fourohfour" */ "./views/404.vue")
-        },
+        { path: '', name: "training", component: TrainingHome },
+        { path: 'about-fknms', name: "about-fknms", component: AboutFKNMS },
       ]
     },
     {
       path: "/admin",
       name: "admin",
-      // route level code-splitting
-      // this generates a separate chunk (admin.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "admin" */ "./views/Admin.vue")
+      component: () => import(/* webpackChunkName: "admin" */ "./views/Admin.vue")
     },
     {
       path: '/quiz/create',
       name: 'quizcreate',
       component: () => import(/* webpackChunkName: "quizcreate" */ "./views/quiz/CreateQuiz.vue")
-    }
+    },
+    {
+      path: "*",
+      name: "four-oh-four",
+      component: () => import(/* webpackChunkName: "fourohfour" */ "./views/404.vue")
+    },
   ]
 });
