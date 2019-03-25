811e84f79e3dc5300a2c77b3e9d004ca626b380d
Eric R <rasliche@gmail.com>
2019-03-25 11:13:37 -0400

    renderless question component that handles a question and emits answer correctness

--- src/components/quiz/Question.vue
+++ src/components/quiz/Question.vue
@@ -1,15 +1,25 @@
-<template>
-    <div>
-        <h1>This is a question component</h1>
-    </div>
-</template>
-
 <script>
 export default {
-
+    name: "question",
+    props: ['question', 'answers'],
+    render() {
+        return this.$scopedSlots.default({
+            question: this.question,
+            answers: this.answers,
+            handleAnswer: this.handleAnswer
+        })
+    },
+    methods: {
+        handleAnswer(i) {
+            if (this.answers[i].isRight) {
+                console.log("Your answer was right.")
+                this.$emit('correctAnswer')
+            } else {
+                console.log("Your answer was incorrect.")
+                this.$emit('wrongAnswer')
+            }
+            console.log("handled an answer")
+        }
+    }
 }
 </script>
-
-<style>
-
-</style>
