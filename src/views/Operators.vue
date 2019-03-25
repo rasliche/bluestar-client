757b839c84f4614a60c9e7f781d077132c163f23
Eric R <rasliche@gmail.com>
2019-03-25 11:28:25 -0400

    all operators

--- src/views/Operators.vue
+++ src/views/Operators.vue
@@ -1,28 +1,36 @@
 <template>
     <div>
         <h1>All Shops</h1>
-        {{ operators }}
+        <div>
+            <div v-for="operator in operators" :key="operator._id">
+                <router-link :to="{ name: 'operator', params: { slug: operator.slug }}">{{ operator.name }}</router-link>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
 import axios from 'axios'
-import { mapActions } from 'vuex'
 
 export default {
-    methods: {
-        ...mapActions([
-            'fetchAllOperators'
-        ])
-    },
-    mounted() {
-        // this.fetchAllOperators()
-    },
-    computed: {
-        operators() {
-            return this.$store.getters.operators
+    data() {
+        return {
+            operators: []
         }
-    }
+    },
+    async mounted() {
+        try {
+            const { data } = await axios.get('http://localhost:3000/api/operators', {
+                headers: {
+                    'Authorization': `Bearer: ${this.$store.state.user.token}`
+                }
+            })
+            this.operators = data
+        } catch (err) {
+            console.log(err)
+        }
+    },
 }
 </script>
 
