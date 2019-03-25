6057d6ee749fd6ff186e40b2c2e5714667dbe4d2
Eric R <rasliche@gmail.com>
2019-03-25 11:28:17 -0400

    component to show an operator?

--- src/views/Operator.vue
+++ src/views/Operator.vue
@@ -1,15 +1,42 @@
 <template>
     <div>
         <h1>This is a single operator</h1>
+        <p>Name: {{ name }}</p>
     </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-    props: [
-        'name',
-        'slug'
-    ]
+    data() {
+        return {
+            loading: true,
+            name: null,
+            slug: null,
+            programs: []
+        }
+    },
+    watch: {
+        '$route': 'fetchOperator'
+    },
+    methods: {
+        async fetchOperator() {
+            this.loading = true
+            const { data } = await axios.get(`http://localhost:3000/api/operators/${this.$route.params.slug}`, {
+                headers: {
+                    Authorization: `Bearer: ${this.$store.state.user.token}`
+                }
+            })
+            this.name = data.name
+            this.slug = data.slug
+            this.programs = data.programs
+            this.loading = false
+        }
+    },
+    created() {
+        this.fetchOperator()
+    }
     
 }
 </script>
