28739fbbe1a159a3c8045f3500a8286c43d70fbd
Eric R <rasliche@gmail.com>
2019-04-17 14:33:08 -0400

    rename and fix validation elements that show conditionally

--- src/components/auth/Register.vue
+++ src/components/auth/Register.vue
@@ -4,15 +4,20 @@
         <form>
             <div>
                 <label for="name" class="text-blue-darker pr-2">Name</label>
-                <input type="text" name="name" id="name" v-model="$v.name.$model" class="border-blue-lighter border-b-2 pl-2">
+                <input type="text" name="name" id="name" v-model.lazy="$v.name.$model" class="border-blue-lighter border-b-2 pl-2">
+                <p class="text-red" v-if="!$v.name.required">this field is required</p>
             </div>
             <div>
                 <label for="email" class="text-blue-darker pr-2">Email</label>
-                <input type="text" name="email" id="email" v-model="$v.email.$model" class="pl-2 border-b-2 border-blue-lighter">
+                <input type="text" name="email" id="email" v-model.lazy="$v.email.$model" class="pl-2 border-b-2 border-blue-lighter">
+                <p class="text-red" v-if="!$v.email.required">this field is required</p>
+                <p class="text-red" v-if="!$v.email.minLength">Field must have at least {{ $v.email.$params.minLength.min }} characters.</p>
             </div>
             <div>
                 <label for="password" class="text-blue-darker pr-2">Password</label>
-                <input type="text" name="password" id="password" v-model="$v.password.$model" class="pl-2 border-b-2 border-blue-lighter">
+                <input type="text" name="password" id="password" v-model.lazy="$v.password.$model" class="pl-2 border-b-2 border-blue-lighter">
+                <p class="text-red" v-if="!$v.password.required">this field is required</p>
+                <p class="text-red" v-if="!$v.password.minLength">Field must have at least {{ $v.password.$params.minLength.min }} characters.</p>
             </div>
             <!-- <label for="shopChoice" class="w-1/2 text-right pr-2">Choose a Shop to Join:</label>
             <select name="shopChoice" id="shopChoice" class="w-1/2" v-model="shopChoice">
