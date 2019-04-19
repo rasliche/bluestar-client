142e0830c080748bffe80f40ea4ad244c20c28a4
Eric R <rasliche@gmail.com>
2019-04-19 18:17:47 -0400

    show button to edit the lesson page if user isAdmin

--- src/views/lessons/Lesson.vue
+++ src/views/lessons/Lesson.vue
@@ -2,11 +2,13 @@
     <article class="lesson flex flex-wrap justify-center">
         <h1 class="w-5/6 border-blue-lighter border-b-4 mb-4">{{ title }}</h1>
         <editor-content class="w-5/6" :editor="editor" />
+        <button v-if="isAdmin">Edit Lesson</button>
     </article>
 </template>
 
 <script>
 import Api from '@/services/Api'
+import { mapGetters } from 'vuex'
 import { Editor, EditorContent } from 'tiptap'
 
 export default {
@@ -18,6 +20,9 @@ export default {
             title: '',
             editor: null,
         }
+    },
+    computed: {
+        ...mapGetters(['isAdmin'])
     },
     async mounted() {
         const { data } = await Api.get(`/lessons/${this.$route.params.slug}`)
