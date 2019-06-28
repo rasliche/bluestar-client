f5cd1e8ec1b293f5137e5befd78385b7e1c5b260
Eric R <rasliche@gmail.com>
2019-06-28 14:12:08 -0400

    Register component available

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,22 +3,22 @@
     <h1>Blue Star Training</h1>
     <p>Welcome to the Blue Star training. This training is required for your dive/snorkel shop to be recognized as a Blue Star operator.</p>
     <p>Please select your dive/snorkel shop from the drop down menu and enter your shop's password.</p>
-    <TrainingNav />
     <Login />
+    <Register />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import Login from '@/components/auth/Login.vue'
-import TrainingNav from '@/components/TrainingNav.vue'
+import Register from '@/components/auth/Register.vue'
 import { mapState } from 'vuex'
 
 export default {
   name: "home",
   components: {
     Login,
-    TrainingNav,
+    Register,
   },
   computed: {
     ...mapState('user', {
