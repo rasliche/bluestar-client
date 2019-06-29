f265e7965d49334947e916b95e70d799ee9d932e
Eric R <rasliche@gmail.com>
2019-06-29 13:29:18 -0400

    added some margin around the lesson card

--- src/components/lesson/LessonCard.vue
+++ src/components/lesson/LessonCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="max-w-sm rounded overflow-hidden shadow-lg">
+    <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
         <img class="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains">
         <router-link tag="div" :to="{ name: slug}" class="px-6 py-4">
             <div class="font-bold text-xl mb-2">{{ title }}</div>
