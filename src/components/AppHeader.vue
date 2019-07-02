f02a6db47f3b810edfd98e040ad21ba6c1a5cde8
Eric R <rasliche@gmail.com>
2019-07-02 09:37:55 -0400

    Removed links from Header

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,32 +5,6 @@
       class="text-3xl ml-4 mt-2 text-blue-lightest no-underline"
       >Blue Star</router-link
     >
-    <!-- <router-link v-if="!isAuthenticated" to="/login" class="text-blue-lightest">Login</router-link> -->
-    <router-link v-if="isAuthenticated" to="/me" class="text-blue-lightest"
-      >Me</router-link
-    >
-    <a v-if="isAuthenticated" @click="logout" class="text-blue-lightest"
-      >Logout</a
-    >
+    
   </header>
 </template>
-
-<script>
-import { mapGetters } from "vuex";
-import { mapActions } from "vuex";
-
-export default {
-  computed: {
-    ...mapGetters({
-      isAuthenticated: "isAuthenticated"
-    })
-  },
-  methods: {
-    ...mapActions({
-      logout: "logout"
-    })
-  }
-};
-</script>
-
-<style></style>
