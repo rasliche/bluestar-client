fcfbf7132129c907661d290b19a59fec5119ca7a
Eric R <rasliche@gmail.com>
2019-07-17 08:51:33 -0400

    opacity has hover option

--- tailwind.js
+++ tailwind.js
@@ -885,7 +885,7 @@ module.exports = {
     negativeMargin: ["responsive"],
     objectFit: false,
     objectPosition: false,
-    opacity: ["responsive"],
+    opacity: ["responsive", "hover"],
     outline: ["focus"],
     overflow: ["responsive"],
     padding: ["responsive"],
