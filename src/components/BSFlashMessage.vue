1c966edf9a237a7f584f0a6153a89c5625c80fb3
Eric R <rasliche@gmail.com>
2019-07-17 08:51:57 -0400

    Alert uses actions from store module and responds to hovering

--- src/components/BSFlashMessage.vue
+++ src/components/BSFlashMessage.vue
@@ -1,19 +1,24 @@
 <template>
 <div class="fixed pin-r pin-b m-6">
     <div class="bg-red text-red-darkest rounded-lg shadow-md p-6 pr-10 opacity-75 hover:opacity-100" style="min-width: 240px">
-        <button class="cursor-pointer absolute top-0 right-0 py-2 px-3">
+        <button class="cursor-pointer static top-0 right-0 py-2 px-3" @click="clearAlert">
             X
         </button>
         <div class="flex items-center">
-            Oops! Something terrible happened...
+            {{ text }}
         </div>
     </div>
 </div>
 </template>
 
 <script>
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions } = createNamespacedHelpers('alert')
 export default {
     name: 'BSFlashMessage',
-    props: ['type', 'text']
+    props: ['type', 'text'],
+    methods: {
+        ...mapActions(['clearAlert'])
+    }
 }
 </script>
