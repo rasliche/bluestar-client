da6d18d2363356214e6af1fc31bfc39381bd7c63
Eric R <rasliche@gmail.com>
2019-07-21 15:03:07 -0400

    Why is CSS so hard? Modal attempt

---
+++ src/components/Modal.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="fixed pin p-8" style="background-color: rgba(0, 0, 0, 0.5);" v-show="show">
+    <div class="mx-auto mt-8 p-2 rounded-lg bg-white max-w-xl">
+        <h1>Notice you've just have to see!</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut quasi tempore eveniet ex doloribus, ipsam dolores nulla aut dolore minima delectus illum itaque nihil ab beatae sapiente. Consequatur, quidem omnis.</p>
+        <slot></slot>
+        <div class="text-center">
+            <button @click="dismiss" class="bg-blue-light px-4 py-2 border-blue border-2 rounded-lg">Close</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['show'],
+    methods: {
+        dismiss() {
+            this.$emit('close')
+        }
+    }
+}
+</script>
