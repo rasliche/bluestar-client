39528c933abb296e9693a9b439ecf31c5a52b7f4
Eric R <rasliche@gmail.com>
2019-07-25 09:51:44 -0400

    Login form now has validations

--- src/components/auth/Login.vue
+++ src/components/auth/Login.vue
@@ -3,26 +3,28 @@
     <h1 class="">Login</h1>
     <form>
       <div class="m-2">
-        <label for="email" class="text-blue-darker pr-2">Email</label>
+        <label for="femail" class="text-blue-darker pr-2">Email</label>
         <input
           type="text"
-          name="email"
-          id="email"
-          v-model="email"
+          name="femail"
+          id="femail"
+          v-model.lazy="$v.formResponses.email.$model"
           autocomplete="section-login email"
           class="border-blue-lighter border-b-2 pl-2"
         />
+        <p class="text-red text-sm" v-if="!$v.formResponses.email.required">this field is required</p>
       </div>
       <div>
-        <label for="password" class="text-blue-darker pr-2">Password</label>
+        <label for="fpassword" class="text-blue-darker pr-2">Password</label>
         <input
           type="password"
-          name="password"
-          id="password"
-          v-model="password"
+          name="fpassword"
+          id="fpassword"
+          v-model.lazy="$v.formResponses.password.$model"
           autocomplete="section-login current-password"
           class="border-blue-lighter border-b-2 pl-2"
         />
+        <p class="text-red text-sm" v-if="!$v.formResponses.password.required">this field is required</p>
       </div>
 
       <button
@@ -36,18 +38,32 @@
 </template>
 
 <script>
+import { required, } from 'vuelidate/lib/validators'
+
 export default {
   data() {
     return {
-      email: "",
-      password: ""
+      formResponses: {
+        email: null,
+        password: null
+      }
     };
+  },
+  validations: {
+      formResponses: {
+          email: {
+              required,
+          },
+          password: {
+              required,
+          }
+      }
   },
   methods: {
     async submitLogin() {
       const authData = {
-        email: this.email,
-        password: this.password
+        email: this.formResponses.email,
+        password: this.formResponses.password
       };
 
       this.$store.dispatch("login", authData);
