198a43fdc58de8f6ce03c16e45c163b8502b1226
Eric R <rasliche@gmail.com>
2019-07-25 08:04:02 -0400

    escape to close document listener

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -13,12 +13,16 @@
 
 <script>
 export default {
+    props: {
+        show: { required: true},
         preventBackgroundScrolling: { default: true }
     },
     methods: {
         dismiss() {
             this.$emit('close')
         }
+    },
+    watch: {
         show: {
             immediate: true,
             handler: function (show) {
@@ -31,6 +35,15 @@ export default {
             }
     },
     created() {
+        const escapeHandler = (e) => {
+            if (e.key === 'Escape' && this.show) {
+                this.dismiss()
+            }
     }
+        document.addEventListener('keydown', escapeHandler)
+        this.$once('hook:destroyed', () => {
+            document.removeEventListener('keydown', escapeHandler)
+        })
+    },
 }
 </script>
