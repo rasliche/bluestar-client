706b15782d2af98b1a8a62c91674c2560abe9f90
Eric R <rasliche@gmail.com>
2019-07-25 08:06:34 -0400

    emptied slots for development

--- src/components/Modal.vue
+++ src/components/Modal.vue
@@ -1,14 +1,10 @@
 <template>
 <div class="fixed pin p-8" style="background-color: rgba(0, 0, 0, 0.6);" v-show="show">
     <div class="mx-auto mt-8 p-2 rounded-lg bg-white max-w-xl shadow-lg">
-        <slot name="heading">
-            <h1 class="text-center text-2xl text-blue-darkest mb-4">Notice you've just have to see!</h1>
-        </slot>
+        <slot name="heading"></slot>
 
-        <slot name="default">
-            <p class="text-center text-grey-darkest">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut quasi tempore eveniet ex doloribus, ipsam dolores nulla aut dolore minima delectus illum itaque nihil ab beatae sapiente. Consequatur, quidem omnis.</p>
-        </slot>
-        
+        <slot name="default"></slot>
+
         <div class="text-center">
             <button @click="dismiss" class="bg-blue-light px-4 py-2 border-blue border-2 rounded-lg">Close</button>
         </div>
