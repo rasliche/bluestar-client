c9189d62aba43a2edfc47a318492596f7842a8b0
Eric R <rasliche@gmail.com>
2019-08-07 15:52:25 -0400

    Flash messages will have to be defined elsewhere

--- src/main.js
+++ src/main.js
@@ -9,10 +9,8 @@ import "./registerServiceWorker";
 // UI Components
 import BSImage from "@/components/BSImage.vue";
 import BSImageWithBigCaption from "@/components/BSImageWithBigCaption.vue";
-import BSFlashMessage from "@/components/BSFlashMessage.vue";
 Vue.component("BSImage", BSImage);
 Vue.component("BSImageWithBigCaption", BSImageWithBigCaption);
-Vue.component("BSFlashMessage", BSFlashMessage);
 
 Vue.config.productionTip = false;
 
