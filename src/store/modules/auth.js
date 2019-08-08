9c2b605be011ef0d44dba204b498ff22d554c80f
Eric R <rasliche@gmail.com>
2019-08-08 16:07:29 -0400

    handle auto login, logout, and reaching across vuex modules to set and clear state

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -12,98 +12,96 @@ const getters = {
 
 const mutations = {
   setToken: (state, token) => {
-        state.token = token;
-      },
+      state.token = token;
+    },
   clearToken: state => {
-        state.token = null;
-      },
+      state.token = null;
+    },
 }
 
 const actions = {
-    setLogoutTimer: ({ commit }, expirationTime) => {
-        setTimeout(() => {
-          commit("clearAuth");
-        }, expirationTime * 1000);
-      },
-      register: async ({ commit, dispatch }, formData) => {
-        try {
-          const { data } = await Api.post("/users", formData);
-          if (!data.user) {
-            console.log(data);
-            console.log("error'd out");
-            return;
-          }
-          commit("authUser", data);
-          dispatch('alert/setAlert', {
-            type: 'success',
-            text: 'You have created a new account and been logged in.'
-          })
-          const now = new Date();
-          const expirationDate = new Date(now.getTime() + 7200000);
-          localStorage.setItem("bs-auth-time", expirationDate);
-          localStorage.setItem("bs-auth-token", data.token);
-          dispatch("setLogoutTimer", 7200);
-          router.push("/");
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      login: async ({ commit, dispatch }, authData) => {
-        try {
-          const { data } = await Api.post("/auth/login", authData);
-          if (!data.user) {
-            console.log(data);
-            console.log("error'd out");
-            return;
-          }
-          console.log(data);
-          commit("authUser", data);
-          dispatch('alert/setAlert', {
-            type: 'success',
-            text: 'You have been logged in.'
-          })
-          const now = new Date();
-          const expirationDate = new Date(now.getTime() + 7200000);
-          localStorage.setItem("bs-auth-time", expirationDate);
-          localStorage.setItem("bs-auth-token", data.token);
-          dispatch("setLogoutTimer", 7200);
-          router.push("/");
-        } catch (err) {
-          console.log(err);
-        }
-      },
-      tryAutoLogin: async ({ commit, dispatch }) => {
-        const token = localStorage.getItem("bs-auth-token");
-        if (!token) {
-          return;
-        }
-        const expirationDate = new Date(localStorage.getItem("bs-auth-time"));
-        const now = new Date();
-        if (now >= expirationDate) {
-          return;
-        }
-        const { data } = await Api.get("/users/me", {
-          headers: {
-            Authorization: `Bearer: ${token}`
-          }
-        });
-        data.token = token;
-        commit("authUser", data);
-        dispatch('alert/setAlert', {
-          type: 'success',
-          text: 'You have been automatically logged in. Make sure to Logout if this is a shared computer.'
-        })
-      },
-      logout: ({ commit, dispatch }) => {
-        localStorage.removeItem("bs-auth-token");
-        localStorage.removeItem("bs-auth-time");
+  setLogoutTimer: ({ commit }, expirationTime) => {
+      setTimeout(() => {
+        commit('user/clearUser', {}, { root: true })
         commit("clearAuth");
-        dispatch('alert/setAlert', {
-          type: 'success',
-          text: 'You have been logged out.'
-        })
-        router.replace("/login");
-      },  
+      }, expirationTime * 1000);
+    },
+  register: async ({ commit, dispatch }, formData) => {
+    try {
+      const { data } = await Api.post("/users", formData);
+      if (!data.user) {
+        console.log(data);
+        console.log("error'd out");
+        return;
+      }
+      commit("authUser", data);
+      dispatch('alert/setAlert', {
+        type: 'success',
+        text: 'You have created a new account and been logged in.'
+      })
+      const now = new Date();
+      const expirationDate = new Date(now.getTime() + 7200000);
+      localStorage.setItem("bs-auth-time", expirationDate);
+      localStorage.setItem("bs-auth-token", data.token);
+      dispatch("setLogoutTimer", 7200);
+      router.push("/");
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  authUser: async ({ commit, dispatch }, token) => {
+    try {
+      commit("setToken", token);
+      const now = new Date();
+      const expirationDate = new Date(now.getTime() + 7200000);
+      localStorage.setItem("bs-auth-time", expirationDate);
+      localStorage.setItem("bs-auth-token", token);
+      dispatch("setLogoutTimer", 7200);
+      router.push("/");
+    } catch (err) {
+      dispatch('alert/setAlert', {
+        type: 'error',
+        text: 'There was an error logging in. Please try again.'
+      })
+    }
+  },
+  tryAutoLogin: async ({ commit, dispatch }) => {
+    const token = localStorage.getItem("bs-auth-token");
+    if (!token) {
+      return;
+    }
+    const expirationDate = new Date(localStorage.getItem("bs-auth-time"));
+    const now = new Date();
+    if (now >= expirationDate) {
+      return;
+    }
+    const { data } = await Api.get("/users/me", {
+      headers: {
+        Authorization: `Bearer: ${token}`
+      }
+    });
+    commit("setToken", data.token);
+    dispatch(
+      'user/setCurrentUser', 
+      data.user, 
+      { root: true })
+    dispatch('alert/setAlert', {
+      type: 'success',
+      text: 'You have been automatically logged in. Make sure to Logout if this is a shared computer.'
+      }, 
+      { root: true })
+  },
+  logoutUser: ({ commit, dispatch }) => {
+    localStorage.removeItem("bs-auth-token");
+    localStorage.removeItem("bs-auth-time");
+    commit("clearToken");
+    dispatch('user/clearCurrentUser', {}, { root: true })
+    dispatch('alert/setAlert', {
+      type: 'success',
+      text: 'You have been logged out.'
+    }, { root: true })
+    router.replace("/login");
+  },  
 }
 
 export default {
