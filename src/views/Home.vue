6e582f0b1326073b49cd6d23ed03af2632344859
Eric R <rasliche@gmail.com>
2019-08-13 15:08:11 -0400

    Remove alert/modal examples

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,22 +9,9 @@
     </section>
     <section class="w-5/6 pt-4 mx-auto">
       <Component :is="currentComponent" class="mx-auto"></Component>
-      <p class="text-xs pt-4" v-if="currentComponent === 'Login'">Do you need to <span class="underline cursor-pointer" @click="currentComponent = 'Register'">register</span> first?</p>
-      <p class="text-xs pt-4" v-else>If you are registered, you can just <span class="underline cursor-pointer" @click="currentComponent = 'Login'">login</span>.</p>
+      <p class="text-xs pt-4 text-center" v-if="currentComponent === 'Login'">Do you need to <span class="underline cursor-pointer" @click="currentComponent = 'Register'">register</span> first?</p>
+      <p class="text-xs pt-4 text-center" v-else>If you are registered, you can just <span class="underline cursor-pointer" @click="currentComponent = 'Login'">login</span>.</p>
     </section>
-    <!-- <button @click="setAlert({ type: 'warn', text: 'info you should know!'})">Make alert</button> -->
-    <!-- <button @click="showModal = true">Show Modal</button>
-    <Modal 
-      :show="showModal" 
-      @close="showModal = false"
-    >
-      <slot name="heading">
-        <h1 class="text-center text-2xl text-blue-darkest mb-4">Notice you've just have to see!</h1>
-      </slot>
-      <slot>
-        <p class="text-center text-grey-darkest">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut quasi tempore eveniet ex doloribus, ipsam dolores nulla aut dolore minima delectus illum itaque nihil ab beatae sapiente. Consequatur, quidem omnis.</p>
-      </slot>
-    </Modal> -->
   </div>
 </template>
 
