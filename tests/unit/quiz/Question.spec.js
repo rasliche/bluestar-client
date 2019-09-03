f9bb3dfbf3f42dc2dd494dc76bbebc2656ea87db
Eric R <rasliche@gmail.com>
2019-09-03 16:06:51 -0400

    test that Question component handles a correct answer

--- tests/unit/quiz/Question.spec.js
+++ tests/unit/quiz/Question.spec.js
@@ -2,12 +2,63 @@ import { shallowMount } from "@vue/test-utils";
 import Question from "@/components/quiz/Question.vue";
 
 describe("Question.vue", () => {
-  it("renders props.msg when passed", () => {
-    const msg = "new message";
-    const wrapper = shallowMount(Question);
-    expect(wrapper.text()).toMatch();
+  it("emits correctAnswer when given index of correct answer", () => {
+    const wrapper = shallowMount(Question, {
+        propsData: {
+            question: "question text",
+            answers: [
+                {
+                    text: "wrong answer",
+                    isRight: false
+                },
+                {
+                    text: "correct answer",
+                    isRight: true
+                }
+            ],
+            reviewText: 'text',
+            showReviewText: false
+        },
+        scopedSlots: {
+            default: '<p>{{props.index}},{{props.text}}</p>'
+          }
+    });
+    // wrapper.vm.handleAnswer(0)
+    wrapper.vm.handleAnswer(1)
+    // expect(wrapper.emitted().correctAnswer).toBe(2)
+    expect(wrapper.emitted().correctAnswer).toBeTruthy()
+    // expect(wrapper.emitted().correctAnswer[1]).toBe("correctAnswer")
   });
 });
+
+// EXAMPLE QUIZ.questions
+// questions: [
+//     {
+//       text:
+//         "The Florida Keys National Marine Sanctuary is part of a _________ system of marine protected areas, managed by the _________.",
+//       answers: [
+//         {
+//           text:
+//             "statewide, Florida Fish and Wildlife Conservation Commission",
+//           isRight: false
+//         },
+//         { text: "regional, National Park Service", isRight: false },
+//         { text: "national, National Park Service", isRight: false },
+//         {
+//           text:
+//             "regional, National Oceanic and Atmospheric Administration",
+//           isRight: false
+//         },
+//         {
+//           text:
+//             "national, National Oceanic and Atmospheric Administration",
+//           isRight: true
+//         }
+//       ],
+//       theMoreYouKnow:
+//         "The Florida Keys National Marine Sanctuary is one of 13 sanctuaries located across the nation and managed by the National Oceanic and Atmospheric Administration, or NOAA. NOAA is a federal agency housed under the U.S. Department of Commerce. NOAA programs and services assist citizens across the entire nation."
+//     },
+// ]
 
 // import { shallowMount } from "@vue/test-utils";
 // import HelloWorld from "@/components/HelloWorld.vue";
