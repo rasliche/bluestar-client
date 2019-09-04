d524e0ff236120c0c0d25a2560372f15eb5e748d
Eric R <rasliche@gmail.com>
2019-09-03 21:37:58 -0400

    happy path to test emitting the right event

--- tests/unit/quiz/Question.spec.js
+++ tests/unit/quiz/Question.spec.js
@@ -2,33 +2,42 @@ import { shallowMount } from "@vue/test-utils";
 import Question from "@/components/quiz/Question.vue";
 
 describe("Question.vue", () => {
-  it("emits correctAnswer when given index of correct answer", () => {
-    const wrapper = shallowMount(Question, {
-        propsData: {
-            question: "question text",
-            answers: [
-                {
-                    text: "wrong answer",
-                    isRight: false
-                },
-                {
-                    text: "correct answer",
-                    isRight: true
-                }
-            ],
-            reviewText: 'text',
-            showReviewText: false
-        },
-        scopedSlots: {
-            default: '<p>{{props.index}},{{props.text}}</p>'
-          }
+    let wrapper
+
+    beforeEach(() => {
+        wrapper = shallowMount(Question, {
+            propsData: {
+                question: "question text",
+                answers: [
+                    {
+                        text: "wrong answer",
+                        isRight: false
+                    },
+                    {
+                        text: "correct answer",
+                        isRight: true
+                    }
+                ],
+                reviewText: 'text',
+                showReviewText: false
+            },
+            scopedSlots: { // Not sure why we need this. Need to figure it out.
+                default: '<p>{{props.index}},{{props.text}}</p>'
+            }
+        });
+        wrapper.vm.handleAnswer(0)
+        wrapper.vm.handleAnswer(1)
+    })
+
+    it("emits correctAnswer when given index of correct answer", () => {
+      expect(wrapper.emitted().correctAnswer.length).toBe(1)
+      expect(wrapper.emitted().correctAnswer).toBeTruthy()
     });
-    // wrapper.vm.handleAnswer(0)
-    wrapper.vm.handleAnswer(1)
-    // expect(wrapper.emitted().correctAnswer).toBe(2)
-    expect(wrapper.emitted().correctAnswer).toBeTruthy()
-    // expect(wrapper.emitted().correctAnswer[1]).toBe("correctAnswer")
-  });
+    
+    it("emits wrongAnswer when given index of a wrong answer", () => {
+        expect(wrapper.emitted().wrongAnswer.length).toBe(1)
+        expect(wrapper.emitted().wrongAnswer).toBeTruthy()
+    });
 });
 
 // EXAMPLE QUIZ.questions
@@ -59,16 +68,3 @@ describe("Question.vue", () => {
 //         "The Florida Keys National Marine Sanctuary is one of 13 sanctuaries located across the nation and managed by the National Oceanic and Atmospheric Administration, or NOAA. NOAA is a federal agency housed under the U.S. Department of Commerce. NOAA programs and services assist citizens across the entire nation."
 //     },
 // ]
-
-// import { shallowMount } from "@vue/test-utils";
-// import HelloWorld from "@/components/HelloWorld.vue";
-
-// describe("HelloWorld.vue", () => {
-//   it("renders props.msg when passed", () => {
-//     const msg = "new message";
-//     const wrapper = shallowMount(HelloWorld, {
-//       propsData: { msg }
-//     });
-//     expect(wrapper.text()).toMatch(msg);
-//   });
-// });
