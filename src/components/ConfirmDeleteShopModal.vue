4f0760e9d86b346822c3353a1a38ad3d710364e7
Eric R <rasliche@gmail.com>
2019-09-06 17:29:32 -0400

    confirm delete modal composed of a base Modal

---
+++ src/components/ConfirmDeleteShopModal.vue
@@ -0,0 +1,34 @@
+<template>
+<Modal :show="show" @close="cancel">
+    <template v-slot:heading>Delete Shop?</template>
+    <template v-slot:buttons>
+        <button @click="deleteOperator" class="bg-red-light px-4 py-2 border-red border-2 rounded-lg mr-2">
+            Delete {{ operator.name }}
+        </button>
+    </template>
+</Modal>
+</template>
+
+<script>
+import Modal from '@/components/Modal.vue'
+
+export default {
+    components: {
+        Modal,
+    },
+    props: ['show', 'shopId', 'operator'],
+    methods: {
+        cancel() {
+            this.$emit('close')
+        },
+        deleteOperator() {
+            console.log("Baleted.")
+            this.cancel()
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
