85343455a454888cae09f9ed42498b8afa3400a1
Eric R <rasliche@gmail.com>
2019-09-06 13:20:57 -0400

    removed some boilerplate

--- src/store/index.js
+++ src/store/index.js
@@ -17,59 +17,17 @@ export default new Vuex.Store({
     user,
     alert,
   },
-  state: {
+  // state: {
     
-  },
-  getters: {
+  // },
+  // getters: {
     
-  },
-  mutations: {
-    // users
-    recordScore: (state, record) => {
-      // do logic to update a score if needed
-      if (!state.lessonScores) {
-        state.lessonScores = [record];
-      } else {
-        state.lessonScores.push(record);
-      }
-    },
+  // },
+  // mutations: {
     
-  },
-  actions: {    
-    // users
-    submitLessonScore: async ({ commit, state }, record) => {
-      // Check if it is a new score for user
-      console.log("New Record: ", record);
-      let previousLessonRecord;
-      if (state.lessonScores) {
-        previousLessonRecord = state.lessonScores.find(
-          r => r.lessonSlug === record.lessonSlug
-          );
-        }
-        console.log("Previous Lesson: ", previousLessonRecord);
-      if (!previousLessonRecord || previousLessonRecord.score < record.score) {
-        // asynchronously add score to user profile in database
-        const { data } = await Api.put(
-          `/users/${state.userId}/records`,
-          {
-            record: record
-          },
-          {
-            headers: {
-              Authorization: `Bearer: ${state.token}`
-            }
-          }
-          );
-          console.log("New lesson score added.");
-          commit("setUser", data);
-          router.push("/training");
-        } else {
-          // do logic to update a score if previous score was worse
-          // Should have Flash Message showing that nothing was added
-          console.log("No new lesson score added.");
-          router.push("/me");
-        }
-      }
-    },
+  // },
+  // actions: {    
+
+  //   },
   });
   
