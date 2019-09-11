d9d2d68ecb8371eedfb5a9bc964dd61febc8fc59
Eric R <rasliche@gmail.com>
2019-09-10 20:45:50 -0400

    mapGet what we need from the store to show authenticated user info

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -9,10 +9,10 @@
     </section>
     <transition name="fade-fast" mode="out-in">
       <section class="w-5/6 pt-4 mx-auto" v-if="!isAuthenticated">
-      <Component :is="currentComponent" class="mx-auto"></Component>
-      <p class="text-xs pt-4 text-center" v-if="currentComponent === 'Login'">Do you need to <span class="underline cursor-pointer" @click="currentComponent = 'Register'">register</span> first?</p>
-      <p class="text-xs pt-4 text-center" v-else>If you are registered, you can just <span class="underline cursor-pointer" @click="currentComponent = 'Login'">login</span>.</p>
-    </section>
+        <Component :is="currentComponent" class="mx-auto"></Component>
+        <p class="text-xs pt-4 text-center" v-if="currentComponent === 'Login'">Do you need to <span class="underline cursor-pointer" @click="currentComponent = 'Register'">register</span> first?</p>
+        <p class="text-xs pt-4 text-center" v-else>If you are registered, you can just <span class="underline cursor-pointer" @click="currentComponent = 'Login'">login</span>.</p>
+      </section>
       <section class="w-5/6 pt-4 mx-auto" v-else>
         <p class="text-4xl pt-4 text-center">Welcome, {{ name }}.</p>
         <!-- links show based on user permissions and roles -->
@@ -31,6 +31,7 @@
 // @ is an alias to /src
 import Login from "@/components/auth/Login.vue";
 import Register from "@/components/auth/Register.vue";
+import { mapGetters } from "vuex"
 
 export default {
   name: "home",
@@ -44,5 +45,9 @@ export default {
       currentComponent: 'Login'
     }
   },
+  computed: {
+    ...mapGetters('user', ['name']),
+    ...mapGetters('auth', ['isAuthenticated']),
+  }
 };
 </script>
