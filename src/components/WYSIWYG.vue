4ed80f70837041345ff80836013dcec991247a70
Eric R <rasliche@gmail.com>
2019-09-27 22:54:57 -0400

    Basic editor with basic nodes and a menubar

--- src/components/WYSIWYG.vue
+++ src/components/WYSIWYG.vue
@@ -1,15 +1,56 @@
 <template>
-  <div>
-      
+  <div class="editor">
+      <EditorMenuBar :editor="editor" v-slot="{ commands, isActive }">
+          <div>
+            <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
+                Bold
+            </button>
+            <button :class="{ 'is-active': isActive.heading({ level: 1 }) }" @click="commands.heading({ level: 1 })">
+                H1
+            </button>
+            <button :class="{ 'is-active': isActive.heading({ level: 2 }) }" @click="commands.heading({ level: 2 })">
+                H2
+            </button>
+            <button :class="{ 'is-active': isActive.heading({ level: 3 }) }" @click="commands.heading({ level: 3 })">
+                H3
+            </button>
+            <button :class="{ 'is-active': isActive.link() }" @click="commands.link">
+                a
+            </button>
+          </div>
+      </EditorMenuBar>
+      <EditorContent :editor="editor"></EditorContent>
   </div>
 </template>
 
 <script>
-export default {
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap'
+import { Bold, Heading, Link, History } from 'tiptap-extensions'
 
+export default {
+    name: 'wysiwyg',
+    components: {
+        EditorContent,
+        EditorMenuBar
+    },
+    data() {
+        return {
+            editor: new Editor({
+                content: '<p>Default content</p>',
+                extensions: [
+                    new Bold(),
+                    new Heading({
+                        levels: [1,2,3]
+                    }),
+                    new Link(),
+                    new History(),
+                ],
+            })
+        }
+    },
+    beforeDestroy() {
+        // Always destroy your editor instance when it's no longer needed
+        this.editor.destroy()
+    },
 }
 </script>
-
-<style>
-
-</style>
