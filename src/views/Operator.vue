686c0326e15c984a889aedc49e7aa441ffb080de
Eric R <rasliche@gmail.com>
2019-10-30 09:58:01 -0400

    Delete an Operator from their view page

--- src/views/Operator.vue
+++ src/views/Operator.vue
@@ -5,6 +5,17 @@
     <section>
       <p v-for="(value, name) in operatorWithoutInternals" :key="name">{{ name }}: {{ value }}</p>
     </section>
+    <button @click="confirmDeleteModalOpen = true">
+        Delete Shop {{ operator }}
+        <!-- This modal should send a request to archive a shop to the API
+        If successful, remove from the displayed list.
+        what TODO if unsuccessful? -->
+        <ConfirmDeleteShopModal 
+          :show="confirmDeleteModalOpen" 
+          @close="confirmDeleteModalOpen = false"
+          :operator="operator">
+        </ConfirmDeleteShopModal>
+    </button>
   </div>
 </template>
 
