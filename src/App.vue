538b380fd0f269f27fc50af0353dba9c46b76aac
Eric R <rasliche@gmail.com>
2019-10-31 21:32:06 -0400

    Removed the app header and replaced with just the Nav

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
-    <AppHeader id="head" />
+    <AppNav />
 
-    <AppNav id="nav" />
+    <router-view></router-view>
 
-    <router-view id="main" />
-
-    <AppFooter id="foot" />
-
+    <AppFooter />
+    <!-- Notifications and Portals -->
     <portal-target name="modals"></portal-target>
     <transition name="alert-slide">
       <Alert v-if="alert.type" :type="alert.type" :text="alert.text"></Alert>
     </transition>
+
   </div>
 </template>
 
 <script>
 import AppNav from "@/components/AppNav.vue";
-import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import Alert from "@/components/Alert.vue";
 import { createNamespacedHelpers } from 'vuex'
@@ -26,9 +24,8 @@ const { mapState } = createNamespacedHelpers('alert')
 export default {
   components: {
     AppNav,
-    AppHeader,
     AppFooter,
-    Alert
+    Alert,
   },
   created() {
     this.$store.dispatch("auth/tryAutoLogin");
@@ -38,34 +35,6 @@ export default {
   }
 };
 </script>
-
-<style>
-#app {
-  display: grid;
-  grid-template-areas:
-    "head head"
-    "nav main"
-    "nav foot";
-  grid-template-rows: 50px 1fr 30px;
-  grid-template-columns: 150px 1fr;
-}
-
-#app > #head {
-  grid-area: head;
-}
-
-#app > #nav {
-  grid-area: nav;
-}
-
-#app > #foot {
-  grid-area: foot;
-}
-
-#app > #main {
-  grid-area: main;
-}
-</style>
 
 <style src="./styles/styles.css"></style>
 <style src="./styles/transitions.css"></style>
