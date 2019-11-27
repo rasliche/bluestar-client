51b7048126526cd9a3a1aec20ea12b42bcd349b5
Eric R <rasliche@gmail.com>
2019-11-27 11:18:34 -0500

    change path

---
+++ src/components/Alert/Alert.vue
@@ -0,0 +1,51 @@
+<template>
+<div class="fixed pin-r pin-b m-4">
+    <div 
+        :class="[
+            `flex justify-between
+            bg-${alertColor} text-${alertColor}-darkest 
+            rounded-sm shadow-md 
+            p-2
+            opacity-75 hover:opacity-100`
+            ]">
+        <div class="mr-2">
+            {{ text }}
+        </div>
+        <button 
+            :class="[
+                `cursor-pointer p-2 text-sm
+                border border-transparent hover:border-${alertColor}-dark 
+                rounded-sm`]"
+            @click="clearAlert">
+            x
+        </button>
+    </div>
+</div>
+</template>
+
+<script>
+import { createNamespacedHelpers } from 'vuex'
+const { mapActions } = createNamespacedHelpers('alert')
+export default {
+    name: 'Alert',
+    props: {
+        type: {
+            type: String,
+            default: 'warn',
+        },
+        text: {
+            type: String,
+        }
+    },
+    methods: {
+        ...mapActions(['clearAlert'])
+    },
+    computed: {
+        alertColor: function() {
+            if (this.type === 'error') return 'red'
+            if (this.type === 'warn') return 'yellow'
+            if (this.type === 'success') return 'green'
+        }
+    }
+}
+</script>
