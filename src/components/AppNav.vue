8ceaad7e1f78c8fd057ed2d64041b0054d85252e
Eric R <rasliche@gmail.com>
2019-12-01 10:14:32 -0500

    add the app-nav class on the AppNav component to offset the scroll progress bar

--- src/components/AppNav.vue
+++ src/components/AppNav.vue
@@ -1,5 +1,5 @@
 <template>
-  <nav class="flex items-center justify-between flex-wrap p-4 bg-blue-darker">
+  <nav id="app-nav" class="flex items-center justify-between flex-wrap p-4 bg-blue-darker">
     <!-- Desktop Logo -->
     <div class="flex items-center flex-no-shrink text-white mr-6">
       <svg class="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
@@ -67,6 +67,7 @@
 import { mapGetters, mapActions } from "vuex";
 
 export default {
+  name: 'AppNav',
   computed: {
     ...mapGetters('auth', {
       isAuthenticated: "isAuthenticated",
