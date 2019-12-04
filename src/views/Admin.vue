04bbdae5cfe5f1767c86cf51f708aeec86b4e26a
Eric R <rasliche@gmail.com>
2019-12-03 20:58:40 -0500

    Using svg icons instead of text alone

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -53,22 +53,33 @@
             v-for="lesson in lessons" 
             :key="lesson._id"
             class="flex justify-between mt-1 px-2 py-1 hover:bg-blue-lighter rounded">
-            <router-link
-              class="no-underline"
-              :to="{
-                name: 'view-lesson',
-                params: { slug: lesson.slug }
-              }">
-              {{ lesson.title }}
-            </router-link>
-            <router-link
-              class="no-underline"
-              :to="{ 
-                name: 'edit-lesson', 
-                params: { slug: lesson.slug } 
+              <router-link
+                class="no-underline"
+                :to="{
+                  name: 'view-lesson',
+                  params: { slug: lesson.slug }
                 }">
-                Edit
-            </router-link>
+                {{ lesson.title }}
+              </router-link>
+            <div class="actions flex justify-between">
+              <router-link
+                class="no-underline"
+                :to="{ 
+                  name: 'edit-lesson', 
+                  params: { slug: lesson.slug } 
+                  }">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 mr-2 fill-current text-yellow-dark icon-edit">
+                    <path class="primary" d="M4 14a1 1 0 0 1 .3-.7l11-11a1 1 0 0 1 1.4 0l3 3a1 1 0 0 1 0 1.4l-11 11a1 1 0 0 1-.7.3H5a1 1 0 0 1-1-1v-3z"></path>
+                    <rect width="20" height="2" x="2" y="20" class="secondary" rx="1"></rect>
+                  </svg>
+              </router-link>
+              <button>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-6 mr-2 fill-current text-red-dark icon-trash">
+                  <path class="primary" d="M5 5h14l-.89 15.12a2 2 0 0 1-2 1.88H7.9a2 2 0 0 1-2-1.88L5 5zm5 5a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1zm4 0a1 1 0 0 0-1 1v6a1 1 0 0 0 2 0v-6a1 1 0 0 0-1-1z"></path>
+                  <path class="secondary" d="M8.59 4l1.7-1.7A1 1 0 0 1 11 2h2a1 1 0 0 1 .7.3L15.42 4H19a1 1 0 0 1 0 2H5a1 1 0 1 1 0-2h3.59z"></path>
+                </svg>
+              </button>
+            </div>
           </div>
         </div>
         <p v-else>No lessons yet.</p>
