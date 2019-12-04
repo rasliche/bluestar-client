1abbcfa91002486dd75a7ba6c39b5aba39e2593f
Eric R <rasliche@gmail.com>
2019-12-04 15:12:25 -0500

    Validation and extra CreateQuestion to it's own component

--- src/components/quiz/CreateQuiz.vue
+++ src/components/quiz/CreateQuiz.vue
@@ -1,7 +1,7 @@
 <template>
-  <!-- <div class="w-full max-w-xs"> -->
-    <form class="bg-white rounded px-8 pt-6 pb-8 mb-4">
-      <section class="relative mb-6 pb-3">
+  <form class="w-full max-w-3xl bg-white rounded px-8 pt-6 pb-8 mb-4">
+    <div class="flex flex-wrap -mx-3">
+      <section class="relative w-full md:w-5/6 mb-6 px-3 pb-3">
         <label 
           for="title" 
           class="block text-blue-darker font-bold text-sm mb-2">
@@ -11,34 +11,15 @@
           type="text"
           name="title"
           id="title"
-          v-model="$v.quiz.title.$model"
           class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          v-model="$v.quiz.title.$model"
         />
         <p v-if="errors" class="absolute pin-b pin-x">
           <span class="error" v-if="!$v.quiz.title.required">This field is required.</span>
         </p>
       </section>
 
-      <section class="relative mb-6 pb-3">
-        <label 
-          for="description" 
-          class="block text-blue-darker font-bold text-sm mb-2">
-          Quiz Description
-        </label>
-        <input
-          type="text"
-          name="description"
-          id="description"
-          v-model="$v.quiz.description.$model"
-          class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        />
-        <p v-if="errors" class="absolute pin-b pin-x">
-          <span class="error" v-if="!$v.quiz.description.required">This field is required. </span>
-          <span class="error" v-if="!$v.quiz.description.minLength">Field must have at least {{ $v.quiz.description.$params.minLength.min }} characters.</span>
-        </p>
-      </section>
-      
-      <section class="relative mb-6 pb-3">
+      <section class="relative w-full md:w-1/6 mb-6 px-3 pb-3">
         <label 
           for="passingScorePercent" 
           class="block text-blue-darker font-bold text-sm mb-2">
@@ -50,49 +31,46 @@
           max="100"
           name="passingScorePercent"
           id="passingScorePercent"
-          v-model="$v.quiz.passingScorePercent.$model"
           class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          v-model="$v.quiz.passingScorePercent.$model"
         />
+        <p v-if="errors" class="absolute pin-b pin-x">
+          <span class="error" v-if="!$v.quiz.passingScorePercent.required">This field is required. </span>
+        </p>
       </section>
 
-      <section v-if="quiz.questions.length" class="relative mb-6 pb-3">
-        <!-- <label class="block text-blue-darker font-bold text-lg mb-2">
-          Questions
-        </label> -->
+      <section class="relative w-full mb-6 px-3 pb-3">
+        <label 
+          for="description" 
+          class="block text-blue-darker font-bold text-sm mb-2">
+          Quiz Description
+        </label>
+        <input
+          type="text"
+          name="description"
+          id="description"
+          class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          v-model="$v.quiz.description.$model"
+        />
+        <p v-if="errors" class="absolute pin-b pin-x">
+          <span class="error" v-if="!$v.quiz.description.required">This field is required. </span>
+          <span class="error" v-if="!$v.quiz.description.minLength">Field must have at least {{ $v.quiz.description.$params.minLength.min }} characters.</span>
+          <span class="error" v-if="!$v.quiz.description.maxLength">Field must have less than {{ $v.quiz.description.$params.maxLength.max }} characters.</span>
+        </p>
+      </section>
+    </div>
+
+    <div class="flex flex-wrap -mx-3">
+      <CreateQuestion 
+        class="w-full md:w-3/5 mb-6 px-3 pb-3"
+        @validQuestion="addQuestion"></CreateQuestion>
+      <section 
+        v-if="quiz.questions.length" 
+        class="w-full md:w-2/5 mb-6 px-3 pb-3">
         <!-- Accordion these questions -->
-        <div v-for="(question, index) in quiz.questions" :key="index">
-          <p>{{ question.text }}</p>
-          <p
-            :class="{ 'bg-green-lightest': answer.isRight }"
-            v-for="(answer, index) in question.answers"
-            :key="index">
-            <svg v-if="answer.isRight" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 mr-4 fill-current icon-check">
-              <circle cx="12" cy="12" r="10" class="text-green-light"></circle>
-              <path class="text-green-darker" d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"></path>
-            </svg>
-            <svg v-else xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-8 mr-4 fill-current icon-close-circle">
-              <circle cx="12" cy="12" r="10" class="text-red-light"></circle>
-              <path class="text-red-darker" d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"></path>
-            </svg>
-            {{ answer.text }}
-          </p>
-        </div>
-      </section>
-
-      <section class="relative mb-6 pb-3">
-          <section class="relative mb-6 pb-3">
-            <label 
-              for="questiontext"
-              class="block text-blue-darker font-bold text-sm mb-2">
-              Question Text
-            </label>
-            <input
-              type="text"
-              name="questiontext"
-              class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="question.text"/>
-          </section>
-          <div v-for="(answer, index) in question.answers" :key="index">
+        <ol v-for="(question, index) in quiz.questions" :key="index">
+          <li><p>{{ question.text }}</p></li>
+          <ul>
             <p
               :class="{ 'bg-green-lightest': answer.isRight }"
               v-for="(answer, index) in question.answers"
@@ -107,61 +85,27 @@
               </svg>
               {{ answer.text }}
             </p>
-          </div>
-          <section class="relative mb-6 pb-3">
-            <label 
-              for="answertext"
-              class="block text-blue-darker font-bold text-sm mb-2">
-              Answer
-            </label>
-            <input
-              type="text"
-              name="answertext"
-              class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="answer.text"/>
-            <label for="answerisright">
-              <input
-                type="checkbox"
-                name="answerisright"
-                value="isRight"
-                v-model="answer.isRight"/>
-                Correct?
-            </label>
-          </section>
-          <section class="relative mb-6 pb-3">
-            <label 
-              for="themoreyouknow"
-              class="block text-blue-darker font-bold text-sm mb-2">
-              The More You Know 🌈
-            </label>
-            <input
-              type="text"
-              name="themoreyouknow"
-              class="shadow appearance-none rounded border-blue-lighter border w-full py-2 px-3 text-grey-darker mb-3 leading-tight focus:outline-none focus:shadow-outline"
-              v-model="question.theMoreYouKnow"
-            />
-          </section>
-        </section>
-        <button @click.prevent="addAnswer" class="border-2 rounded border-blue">
-          Add Answer
-        </button>
-        <button
-          @click.prevent="addQuestion"
-          :disabled="question.answers.length < 2"
-          class="border-2 rounded border-blue"
-        >
-          Add Question
-        </button>
 
-      <button @click.prevent="submitQuiz" class="border-2 rounded border-blue">
+          </ul>
+        </ol>
+      </section>
+    </div>
+
+
+    <section class="flex w-full items-center justify-between">
+      <button 
+        @click.prevent="submitQuiz" 
+        class="p-2 rounded mx-auto bg-blue hover:bg-blue-dark text-white focus:outline-none focus:shadow-outline">
         Create
       </button>
-    </form>
+    </section>
+  </form>
   <!-- </div> -->
 </template>
 
 <script>
 import Api from '../../services/Api'
+import CreateQuestion from '@/components/quiz/CreateQuestion'
 import {
   required,
   minLength,
@@ -171,6 +115,9 @@ import {
 
 export default {
   name: "CreateQuiz",
+  components: { 
+    CreateQuestion
+  },
   data() {
     return {
       feedback: null,
@@ -182,17 +129,8 @@ export default {
         description: "",
         passingScorePercent: 100,
         questions: [],
-        lessonSlug: ''
+        // lessonSlug: ''
       },
-      question: {
-        text: "",
-        answers: [],
-        theMoreYouKnow: ""
-      },
-      answer: {
-        text: "",
-        isRight: false
-      }
     };
   },
   validations: {
@@ -202,38 +140,21 @@ export default {
       },
       description: {
         required,
+        minLength: minLength(15),
         maxLength: maxLength(255),
       },
       passingScorePercent: {
         required,
       },
-    }
+    },
   },
   methods: {
-    addQuestion() {
-      if (this.question.answers.length > 1) {
-        this.quiz.questions.push({
-          text: this.question.text,
-          answers: this.question.answers,
-          theMoreYouKnow: this.question.theMoreYouKnow
-        });
-        this.question.text = "";
-        this.question.answers = [];
-        this.question.theMoreYouKnow = "";
-      }
+    addQuestion(question) {
+      this.quiz.questions.push(question);
       console.log("added a question");
     },
     removeQuestion(i) {
       console.log("removed question", i);
-    },
-    addAnswer() {
-      this.question.answers.push({
-        text: this.answer.text,
-        isRight: this.answer.isRight
-      });
-      this.answer.text = "";
-      this.answer.isRight = false;
-      console.log("added an answer");
     },
     removeAnswer(i) {
       console.log("removed question", i);
