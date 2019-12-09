1bb3b5623bf74ca9772cbfadd68a5258901a902b
Eric R <rasliche@gmail.com>
2019-12-09 08:52:16 -0500

    allow background scrolling on modal

--- src/views/lesson/EditLesson.vue
+++ src/views/lesson/EditLesson.vue
@@ -75,7 +75,8 @@
         Details
         <Modal 
             :show="lessonDetailModalOpen" 
-            @close="lessonDetailModalOpen = false">
+            @close="lessonDetailModalOpen = false"
+            :preventBackgroundScrolling="false">
             <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
                 <section class="relative mb-6 pb-3">
                     <label 
