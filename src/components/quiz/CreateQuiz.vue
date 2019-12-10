67a84cdaeb0bacd425abb2806e0f4b1ae8afc800
Eric R <rasliche@gmail.com>
2019-12-10 13:55:53 -0500

    comments styling and commit validation logic

--- src/components/quiz/CreateQuiz.vue
+++ src/components/quiz/CreateQuiz.vue
@@ -1,7 +1,7 @@
 <template>
   <form class="w-full max-w-3xl bg-white rounded px-8 pt-6 pb-8 mb-4">
     <div class="flex flex-wrap -mx-3">
-      <section class="relative w-full md:w-5/6 mb-6 px-3 pb-3">
+      <!-- <section class="relative w-full md:w-5/6 mb-6 px-3 pb-3">
         <label 
           for="title" 
           class="block text-blue-darker font-bold text-sm mb-2">
@@ -17,7 +17,7 @@
         <p v-if="errors" class="absolute pin-b pin-x">
           <span class="error" v-if="!$v.quiz.title.required">This field is required.</span>
         </p>
-      </section>
+      </section> -->
 
       <section class="relative w-full md:w-1/6 mb-6 px-3 pb-3">
         <label 
@@ -39,7 +39,7 @@
         </p>
       </section>
 
-      <section class="relative w-full mb-6 px-3 pb-3">
+      <!-- <section class="relative w-full mb-6 px-3 pb-3">
         <label 
           for="description" 
           class="block text-blue-darker font-bold text-sm mb-2">
@@ -57,13 +57,14 @@
           <span class="error" v-if="!$v.quiz.description.minLength">Field must have at least {{ $v.quiz.description.$params.minLength.min }} characters.</span>
           <span class="error" v-if="!$v.quiz.description.maxLength">Field must have less than {{ $v.quiz.description.$params.maxLength.max }} characters.</span>
         </p>
-      </section>
+      </section> -->
     </div>
 
     <div class="flex flex-wrap -mx-3">
       <CreateQuestion 
         class="w-full md:w-2/3 mb-6 px-3 pb-3"
-        @validQuestion="addQuestion"></CreateQuestion>
+        @validQuestion="addQuestion"
+      ></CreateQuestion>
       <section 
         v-if="quiz.questions.length" 
         class="w-full md:w-1/3 mb-6 px-3 pb-3">
@@ -72,7 +73,8 @@
           <li v-for="(question, index) in quiz.questions" :key="index">
             <p>{{ question.text }}</p>
             <ul>
-              <p
+              <li
+                class="list-reset"                
                 :class="{ 'bg-green-lightest': answer.isRight }"
                 v-for="(answer, index) in question.answers"
                 :key="index">
@@ -84,23 +86,22 @@
                   <circle cx="12" cy="12" r="10" class="text-red-light"></circle>
                   <path class="text-red-darker" d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"></path>
                 </svg>
-                {{ answer.text }}
-              </p>
-
+                <p class="inline">{{ answer.text }}</p>
+              </li>
+              <p>{{ question.theMoreYouKnow }}</p>
             </ul>
           </li>
         </ol>
       </section>
     </div>
 
-
-    <section class="flex w-full items-center justify-between">
+    <!-- <section class="flex w-full items-center justify-between">
       <button 
         @click.prevent="submitQuiz" 
         class="p-2 rounded mx-auto bg-blue hover:bg-blue-dark text-white focus:outline-none focus:shadow-outline">
-        Create
+        Save Quiz
       </button>
-    </section>
+    </section> -->
   </form>
   <!-- </div> -->
 </template>
@@ -120,6 +121,7 @@ export default {
   components: { 
     CreateQuestion
   },
+  props: ['lessonId'],
   data() {
     return {
       feedback: null,
@@ -155,14 +157,34 @@ export default {
       this.quiz.questions.push(question);
       console.log("added a question");
     },
-    removeQuestion(i) {
+    removeQuestion(question) {
       console.log("removed question", i);
     },
-    removeAnswer(i) {
-      console.log("removed question", i);
-    },
-    submitQuiz() {
-      console.log(this.quiz);
+    async submitQuiz() {
+      this.formTouched = !this.$v.quiz.$anyDirty;
+      this.errors = this.$v.quiz.$anyError;
+      this.uiState = 'submit clicked'
+      if (this.errors === false && this.formTouched === false) {
+        const quizData = {
+          title: this.quiz.title,
+          description: this.quiz.description,
+          passingScorePercent: this.quiz.passingScorePercent,
+          questions: this.quiz.questions,
+          lessonId: this.lessonId,
+        }
+        console.log(quizData)
+        this.uiState = 'quiz submitted';
+        try {
+          const { data } = await Api.post('/quizzes', quizData)
+          this.setAlert({
+            type: 'success',
+            text: 'Quiz created.'
+          })
+          console.log(data)
+        } catch (error) {
+          this.uiState = "error"
+        }
+      }
     }
   }
 };
