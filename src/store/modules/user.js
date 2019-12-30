06cec3c382c05658703a46fcb628504bcfcdf207
Eric R <rasliche@gmail.com>
2019-12-29 20:12:32 -0500

    Some whitespace cleanup

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -2,83 +2,83 @@ import Api from "@/services/Api";
 import router from "@/router";
 
 const state = {
-    userId: null,
-    name: null,
-    email: null,
-    operators: [],
-    lessonScores: [],
-    isAdmin: false
+  userId: null,
+  name: null,
+  email: null,
+  operators: [],
+  lessonScores: [],
+  isAdmin: false
 }
 
 const getters = {
-    userId: state => state.userId,
-    name: state => state.name,
-    email: state => state.email,
-    operators: state => state.operators,
-    lessonScores: state => state.lessonScores,
-    isAdmin: state => state.isAdmin,
+  userId: state => state.userId,
+  name: state => state.name,
+  email: state => state.email,
+  operators: state => state.operators,
+  lessonScores: state => state.lessonScores,
+  isAdmin: state => state.isAdmin,
 }
 
 const mutations = {
-    setUser: (state, user) => {
-        state.userId = user._id;
-        state.name = user.name;
-        state.email = user.email;
-        state.operators = user.operators;
-        state.lessonScores = user.lessonScores;
-        state.isAdmin = user.isAdmin;
-      },
-      clearUser: (state) => {
-        state.userId = null;
-        state.name = "";
-        state.email = "";
-        state.operators = [];
-        state.lessonScores = [];
-        state.isAdmin = false;
-      },
+  setUser: (state, user) => {
+    state.userId = user._id;
+    state.name = user.name;
+    state.email = user.email;
+    state.operators = user.operators;
+    state.lessonScores = user.lessonScores;
+    state.isAdmin = user.isAdmin;
+  },
+  clearUser: (state) => {
+    state.userId = null;
+    state.name = "";
+    state.email = "";
+    state.operators = [];
+    state.lessonScores = [];
+    state.isAdmin = false;
+  },
 }
 
 const actions = {
-    setCurrentUser: ({ commit }, user) => {
-        commit('setUser', user)
-    },
-    clearCurrentUser: ({ commit }) => {
-        commit('clearUser')
-    },
-    // users
-    submitLessonScore: async ({ dispatch, commit, getters, rootGetters }, record) => {
-        // Check if it is a new score for user
-        console.log("New Record: ", record);
-        let previousLessonRecord;
-        if (getters['user/lessonScores']) {
-          previousLessonRecord = getters['user/lessonScores'].find(
-            r => r.lessonId === record.lessonId
-            );
+  setCurrentUser: ({ commit }, user) => {
+    commit('setUser', user)
+  },
+  clearCurrentUser: ({ commit }) => {
+    commit('clearUser')
+  },
+  // users
+  submitLessonScore: async ({ dispatch, commit, getters, rootGetters }, record) => {
+    // Check if it is a new score for user
+    console.log("New Record: ", record);
+    let previousLessonRecord;
+    if (getters['user/lessonScores']) {
+      previousLessonRecord = getters['user/lessonScores'].find(
+        r => r.lessonId === record.lessonId
+        );
+      }
+    if (previousLessonRecord) console.log("Previous Lesson: ", previousLessonRecord);
+    if (!previousLessonRecord || previousLessonRecord.score < record.score) {
+      // asynchronously add score to user profile in database
+      const { data } = await Api.put(
+        `/users/${rootGetters['user/userId']}/records`, record, // TODO: Why do I need rootGetters here?
+        {
+          headers: {
+            Authorization: `Bearer: ${rootGetters['auth/token']}`
           }
-        if (previousLessonRecord) console.log("Previous Lesson: ", previousLessonRecord);
-        if (!previousLessonRecord || previousLessonRecord.score < record.score) {
-          // asynchronously add score to user profile in database
-          const { data } = await Api.put(
-            `/users/${rootGetters['user/userId']}/records`, record, // TODO: Why do I need rootGetters here?
-            {
-              headers: {
-                Authorization: `Bearer: ${rootGetters['auth/token']}`
-              }
-            });
-            // dispatch('alert/setAlert', {
-            //   type: 'success',
-            //   text: `Updated lesson score.`
-            //   // text: `Updated ${record.lessonName} score to ${record.score}.`
-            // });
-            commit("setUser", data);
-            router.push("/training");
-          } else {
-            // do logic to update a score if previous score was worse
-            // Should have Flash Message showing that nothing was added
-            console.log("No new lesson score added.");
-            router.push("/me");
-          }
-        }
+        });
+        // dispatch('alert/setAlert', {
+        //   type: 'success',
+        //   text: `Updated lesson score.`
+        //   // text: `Updated ${record.lessonName} score to ${record.score}.`
+        // });
+        commit("setUser", data);
+        router.push("/training");
+      } else {
+        // do logic to update a score if previous score was worse
+        // Should have Flash Message showing that nothing was added
+        console.log("No new lesson score added.");
+        router.push("/me");
+      }
+    }
 }
 
 export default {
