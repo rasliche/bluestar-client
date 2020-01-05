f31ee55108151a1eee6c80249ac325d5437a1040
Eric R <rasliche@gmail.com>
2020-01-05 17:27:43 -0500

    Reworked the lesson card to be dynamic and reusable

--- src/components/lesson/BSLessonCard.vue
+++ src/components/lesson/BSLessonCard.vue
@@ -1,41 +1,52 @@
 <template>
-  <div class="max-w-sm rounded overflow-hidden shadow-lg m-2">
-    <router-link tag="div" :to="{ name: 'view-lesson', params: { id } }" class="px-6 py-4 cursor-pointer">
+  <BaseCard class="w-64 mt-4 mx-2 rounded shadow">
+    <router-link 
+      tag="div" 
+      :to="{ name: 'view-lesson', params: { lessonId: lesson._id } }" 
+      class="cursor-pointer">
       <img
-        class="w-full object-cover"
-        :src="thumbnail"
-        alt=""
+        class="h-48 w-auto"
+        :src="lesson.coverPhoto.location"
+        :alt="lesson.coverPhoto.altDescription"
       />
-      <div class="font-bold text-xl mb-2">{{ title }}</div>
-      <p class="text-grey-darker text-base">
-        {{ description }}
-      </p>
+      <div class="text-center font-bold text-lg">{{ lesson.title }}</div>
     </router-link>
-    <div v-if="programs.length" class="px-6 py-4">
+    <p class="text-grey-darker text-sm">
+      {{ lesson.description }}
+    </p>
+    <div v-if="lesson.programs.length" class="px-6 py-4">
       <span
-        v-for="program in programs"
+        v-for="program in lesson.programs"
         :key="program._id"
-        class="inline-block bg-blue-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">
-          #{{ program.name }}
+        class="inline-block bg-blue-lighter rounded-full px-3 py-1 text-xs font-semibold tracking-wide text-grey-darker">
+          {{ program.name }}
         </span>
     </div>
-  </div>
+  </BaseCard>
 </template>
 
 <script>
+import BaseCard from '@/components/BaseUI/BaseCard'
+
 export default {
     name: 'BSLessonCard',
+    components: {
+      BaseCard,
+    },
     props: {
-      id: String,
-      title: String,
-      programs: Array, 
-      thumbnail: {
-        type: String,
-        default: 'https://floridakeys.noaa.gov/onthewater/bs_logomed.jpg'
-      }, 
-      description: String
+      lesson: {
+        _id: String,
+        title: String,
+        programs: Array, 
+        description: String,
+        coverPhoto: {
+          location: {
+            type: String,
+            default: 'https://floridakeys.noaa.gov/onthewater/bs_logomed.jpg'
+          },
+          altDescription: String,
+        },
+      },
     }
 }
 </script>
-
-<style scoped></style>
