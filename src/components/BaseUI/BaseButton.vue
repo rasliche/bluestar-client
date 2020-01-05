3c5bf43f7c8791e77b01999b52ada4fa639fba64
Eric R <rasliche@gmail.com>
2020-01-05 17:23:23 -0500

    Gettin after that design system yo!

--- src/components/BaseUI/BaseButton.vue
+++ src/components/BaseUI/BaseButton.vue
@@ -1,6 +1,8 @@
 <template>
-  <button>
-      <slot></slot>
+  <button
+    type="button"
+    class="p-2 rounded mx-auto focus:outline-none focus:shadow-outline">
+    <slot>Click Me!</slot>
   </button>
 </template>
 
---
+++ src/components/BaseUI/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white border rounded-lg overflow-hidden">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'BaseCard',
+}
+</script>
+
+<style>
+
+</style>
