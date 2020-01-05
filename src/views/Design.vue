5060f509e34f6f6677f02f3bdc1d8ff1099e9146
Eric R <rasliche@gmail.com>
2020-01-05 17:28:15 -0500

    Design page updates

--- src/views/Design.vue
+++ src/views/Design.vue
@@ -1,29 +1,62 @@
 <template>
-  <div>
-    <h1>Design Components</h1>
-    <section id="BSImage">
-      <h2>BSImage</h2>
-      <BSImage />
+  <main>
+    <h1 class="page-heading">
+      Design
+    </h1>
+    <section class="mt-4">
+      <h2>Tokens</h2>
+      <p class="leading-normal">
+        Use this area to describe design tokens: colors, font weights, spacing, tone.
+      </p>
     </section>
-    <section id="BSImageWithBigCaption">
-      <h2>BSImageWithBigCaption</h2>
-      <BSImageWithBigCaption>
-        Here is a caption in a slot.
-      </BSImageWithBigCaption>
+    <section class="mt-4">
+      <h2>Page Headings</h2>
+      <p class="leading-normal">
+        The top-level heading above uses a <pre class="inline-block">.page-heading</pre> class that combines
+        tailwind css classes in the <pre class="inline-block">App.vue</pre> style section. This is
+        used as the main heading for a view, that is, a top level route component.
+      </p>
     </section>
-  </div>
+    <section class="mt-4">
+      <h2>Subheading for Sections</h2>
+      <p class="leading-normal">
+        The text above is wrapped in a section with no styling, as are all the following
+        sections of the document.
+      </p>
+    </section>
+    <section class="mt-4">
+      <h2>Buttons</h2>
+      <p class="leading-normal">
+        Buttons can come in several different flavors. Primary, Secondary, Danger, are the main ones.
+      </p>
+      <div class="flex justify-around">
+        <BaseButton>Base Button</BaseButton>
+        <BaseButton class="bg-blue hover:bg-blue-dark text-white">Primary</BaseButton>
+        <BaseButton class="border bg-grey-lightest hover:bg-grey-light">Secondary</BaseButton>
+        <BaseButton class="bg-red hover:bg-red-dark text-white">Danger</BaseButton>
+      </div>
+    </section>
+    <section class="mt-4">
+      <h2>Cards</h2>
+      <p class="leading-normal">
+        The <pre class="inline-block">BaseCard</pre> component is a very unopinionated wrapper that simply gives a white 
+        background, 1px border, large rounded corners, and hides overflow to keep content within.
+      </p>
+      <div class="flex justify-around">
+        <BaseCard>Primary</BaseCard>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
-import BSImage from "@/components/BSImage.vue";
-import BSImageWithBigCaption from "@/components/BSImageWithBigCaption.vue";
+import BaseButton from '@/components/BaseUI/BaseButton.vue'
+import BaseCard from '@/components/BaseUI/BaseCard.vue'
 
 export default {
-  components: {
-    BSImage,
-    BSImageWithBigCaption
+  components: { 
+    BaseButton,
+    BaseCard
   }
-};
+}
 </script>
-
-<style></style>
