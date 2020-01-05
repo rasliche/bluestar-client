c4b5116934589fece49dbe4c65e7aa8ef986b8e6
Eric R <rasliche@gmail.com>
2020-01-05 17:24:00 -0500

    Dynamic footer

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,5 +1,5 @@
 <template>
     <footer class="text-center text-xs">
-      <p class="border-t border-blue-light px-2 pt-2 mt-2">Made with 🌊 in 2019 by FKNMS</p>
+      <p class="border-t border-blue-light px-2 pt-2 mt-2">Made with 🌊 | {{  new Date().getFullYear() }} FKNMS</p>
     </footer>
 </template>
