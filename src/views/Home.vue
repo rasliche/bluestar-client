ccd5b85b4c9953ec4c6fac7b10ea7fbb6d344729
Eric R <rasliche@gmail.com>
2020-02-05 15:21:27 -0500

    Remove dumb dive flag svg

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,26 +7,6 @@
         Welcome to the Blue Star training. This training is required for your
         dive/snorkel shop to be recognized as a Blue Star operator.
       </p>
-      <svg
-        width="400"
-        height="400"
-        version="1.1"
-        xmlns="http://www.w3.org/2000/svg"
-      >
-        <!-- Dive flag triangles -->
-        <polygon points="50 130 50 300 320 300" stroke="red" fill="red" />
-        <polygon points="80 100 350 100 350 270" stroke="red" fill="red" />
-        <!-- Dive flag border -->
-        <rect
-          x="50"
-          y="100"
-          width="300"
-          height="200"
-          stroke="lightblue"
-          fill="transparent"
-          stroke-width="5"
-        />
-      </svg>
     </section>
     <!-- These two elements have the same tag name, so they need the 'key' attribute so Vue can tell them apart and transition -->
     <transition name="fade" mode="out-in">
@@ -63,10 +43,9 @@
       </section>
       <section
         v-else
-        key="info"
+        key="loggedInActions"
         class="sm:w-5/6 md:w-3/4 lg:w-1/2 w-full mx-auto px-2"
       >
-        <p class="text-4xl pt-4 text-center">Welcome, {{ name }}.</p>
         <!-- links show based on user permissions and roles -->
         <div class="flex">
           <router-link class="w-1/2" :to="{ name: 'me' }">
