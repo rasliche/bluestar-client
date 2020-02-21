e323ba3292241d8c77767377a55ccd3a4b204c5a
Eric R <rasliche@gmail.com>
2020-02-21 10:24:29 -0500

    Rename NotFound view

---
+++ src/views/NotFound.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PageHeading>Error - 404</PageHeading>
+    <p>Page not found</p>
+  </div>
+</template>
+
+<script>
+import { PageHeading } from '@/components/BaseUI'
+
+export default {
+  name: "NotFound",
+  components: {
+    PageHeading
+  }
+}
+</script>
