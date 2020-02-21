241607d699ade6c26fab5d72fa46e2e50b1f0d81
Eric R <rasliche@gmail.com>
2020-02-21 10:23:13 -0500

    Export BaseUI components

---
+++ src/components/BaseUI/index.js
@@ -0,0 +1,15 @@
+// Buttons
+export { default as ButtonBase } from './ButtonBase.vue'
+export { default as ButtonDanger } from './ButtonDanger.vue'
+export { default as ButtonInfo } from './ButtonInfo.vue'
+export { default as ButtonPrimary } from './ButtonPrimary.vue'
+export { default as ButtonSecondary } from './ButtonSecondary.vue'
+
+// Cards
+export { default as CardBase } from './CardBase.vue'
+
+// Modals
+export { default as Modal } from './Modal.vue'
+
+// Typography
+export { default as PageHeading } from './PageHeading.vue'
