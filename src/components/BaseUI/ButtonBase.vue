2ad1365c3b0aa159d870f785e2839b4a3f5211e5
Eric R <rasliche@gmail.com>
2020-02-21 10:31:36 -0500

    Remove margin from ButtonBase.vue

--- src/components/BaseUI/ButtonBase.vue
+++ src/components/BaseUI/ButtonBase.vue
@@ -2,7 +2,7 @@
   <button
     v-on="$listeners"
     type="button"
-    class="p-2 rounded-lg mx-auto focus:outline-none focus:shadow-outline no-underline"
+    class="p-2 rounded-lg focus:outline-none focus:shadow-outline no-underline"
   >
     <slot>Click Me!</slot>
   </button>
