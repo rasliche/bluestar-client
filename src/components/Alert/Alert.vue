c831c1d2435eaa07d163118a88f53f27793cfdf0
Eric <rasliche@gmail.com>
2020-03-05 12:51:24 -0500

    Install PurgeCSS PostCSS plugin (#38)
    Installed and followed guidelines on TailwindCSS docs.
    Purges on production only.

--- src/components/Alert/Alert.vue
+++ src/components/Alert/Alert.vue
@@ -2,11 +2,19 @@
   <div class="fixed right-0 bottom-0 m-4" role="alert">
     <div
       class="flex justify-between rounded-sm shadow-md p-2 opacity-75 hover:opacity-100"
-      :class="[`bg-${alertColor}-200 text-${alertColor}-900`]"
+      :class="[
+        { 'bg-green-200 text-green-900': type === 'success' },
+        { 'bg-yellow-200 text-yellow-900': type === 'warn' },
+        { 'bg-red-200 text-red-900': type === 'error' }
+        ]"
     >
       <button
         class="cursor-pointer m-2 text-sm border border-transparent rounded-sm"
-        :class="[`hover:border-${alertColor}-600`]"
+        :class="[
+          {'hover:border-green-600': type === 'success'},
+          {'hover:border-green-600': type === 'warn'},
+          {'hover:border-green-600': type === 'error'}
+          ]"
         @click="clearAlert"
       >
         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-8 w-8 fill-current"><title>Dismiss</title><path fill-rule="evenodd" d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"></path></svg>
@@ -30,18 +38,11 @@ export default {
     },
     text: {
       type: String,
-      default: 'Alert'
+      default: 'Alert!'
     }
   },
   methods: {
     ...mapActions(['clearAlert'])
-  },
-  computed: {
-    alertColor() {
-      if (this.type === 'error') return 'red'
-      if (this.type === 'warn') return 'yellow'
-      if (this.type === 'success') return 'green'
-    }
   }
 }
 </script>
