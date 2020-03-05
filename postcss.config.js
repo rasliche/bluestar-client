2e6c9210bf63b2d9906ddce25f1bb744972ae2bf
Eric R <rasliche@gmail.com>
2020-03-05 12:50:51 -0500

    Install PurgeCSS PostCSS plugin
    Installed and followed guidelines on TailwindCSS docs.
    Purges on production only.

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,25 @@
+// postcss.config.js
+const purgecss = require('@fullhuman/postcss-purgecss')({
+
+  // Specify the paths to all of the template files in your project 
+  content: [
+    './src/**/*.html',
+    './src/**/*.vue',
+    './src/**/*.jsx',
+    // etc.
+  ],
+
+  // Include any special characters you're using in this regular expression
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+})
+
 module.exports = {
   plugins: [
     require('tailwindcss'),
     require('autoprefixer'),
-    require('postcss-nested')({ bubble: ['apply'] })
+    require('postcss-nested')({ bubble: ['apply'] }),
+    ...process.env.NODE_ENV === 'production'
+      ? [purgecss]
+      : []
   ]
 }
