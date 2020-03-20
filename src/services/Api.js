cb858a9cea1ffc30a3eb988acc412d8260aa973b
Eric R <rasliche@gmail.com>
2020-03-20 14:15:16 -0400

    Add some axios defaults to Api

--- src/services/Api.js
+++ src/services/Api.js
@@ -1,9 +1,14 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: process.env.VUE_APP_AXIOS_BASE_URL
+  baseURL: process.env.VUE_APP_AXIOS_BASE_URL,
+  withCredentials: false,
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json'
+  }
 })
 
-// TODO: Create 401 and 403 response interceptors
+// TODO: Create 401 and 403 response interceptors... in Vuex/store?
 
 export default instance
