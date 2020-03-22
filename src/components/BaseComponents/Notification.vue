b882b5471083a208ab92829cec03c82b19774c7c
Eric R <rasliche@gmail.com>
2020-03-22 10:49:38 -0400

    Remove the notification.id from the template

--- src/components/BaseComponents/Notification.vue
+++ src/components/BaseComponents/Notification.vue
@@ -19,7 +19,7 @@
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="h-8 w-8 fill-current"><title>Dismiss</title><path fill-rule="evenodd" d="M15.78 14.36a1 1 0 0 1-1.42 1.42l-2.82-2.83-2.83 2.83a1 1 0 1 1-1.42-1.42l2.83-2.82L7.3 8.7a1 1 0 0 1 1.42-1.42l2.83 2.83 2.82-2.83a1 1 0 0 1 1.42 1.42l-2.83 2.83 2.83 2.82z"></path></svg>
     </button>
     <div class="mr-2">
-      {{ notification.id}} - {{ notification.text }}
+      {{ notification.text }}
     </div>
   </div>
 </template>
