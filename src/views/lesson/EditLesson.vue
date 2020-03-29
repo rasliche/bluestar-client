83651b5b86d21151a96dedaa1ba3e8ac096fed05
Eric R <rasliche@gmail.com>
2020-03-29 19:52:46 -0400

    Refactor to use BaseInputs

--- src/views/lesson/EditLesson.vue
+++ src/views/lesson/EditLesson.vue
@@ -2,33 +2,9 @@
   <main class="lesson">
     <PageHeading>Edit Lesson</PageHeading>
     <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <section class="relative mb-6 pb-3">
-        <label for="title" class="block text-blue-800 font-bold text-sm mb-2"
-          >Lesson Title</label
-        >
-        <input
-          id="title"
-          v-model="lesson.title"
-          type="text"
-          name="title"
-          class="shadow appearance-none rounded border-blue-200 border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </section>
-      <section class="relative mb-6 pb-3">
-        <label
-          for="description"
-          class="block text-blue-800 font-bold text-sm mb-2"
-          >Short Description</label
-        >
-        <input
-          id="description"
-          v-model="lesson.description"
-          type="text"
-          spellcheck
-          name="description"
-          class="shadow appearance-none rounded border-blue-200 border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </section>
+      <BaseInput label="Lesson Title" type="text" v-model="lesson.title"></BaseInput>
+      <BaseInput label="Short Description" type="text" v-model="lesson.description"></BaseInput>
+
       <section class="relative mb-6 pb-3">
         <div v-for="program in programOptions" :key="program._id">
           <label
@@ -44,20 +20,8 @@
           </label>
         </div>
       </section>
-      <section class="relative mb-6 pb-3">
-        <label
-          for="cover-photo"
-          class="block text-blue-800 font-bold text-sm mb-2"
-          >Cover Photo</label
-        >
-        <input
-          id="cover-photo"
-          v-model="lesson.coverPhoto.location"
-          type="text"
-          name="cover-photo"
-          class="shadow appearance-none rounded border-blue-200 border w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-        />
-      </section>
+      
+      <BaseInput label="Cover Photo" type="text" v-model="lesson.coverPhoto.location"></BaseInput>
       <ButtonPrimary @click="saveLessonDetails">
         Save Details
       </ButtonPrimary>
@@ -68,66 +32,31 @@
     <editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
       <div class="flex justify-between">
         <div class="menubar flex justify-between">
-          <button
-            :class="{ 'is-active': isActive.bold() }"
-            @click="commands.bold"
-          >
+          <button :class="{ 'is-active': isActive.bold() }" @click="commands.bold">
             Bold
           </button>
-          <button
-            :class="{ 'is-active': isActive.italic() }"
-            @click="commands.italic"
-          >
+          <button :class="{ 'is-active': isActive.italic() }" @click="commands.italic">
             Italic
           </button>
-          <button
-            :class="{ 'is-active': isActive.underline() }"
-            @click="commands.underline"
-          >
+          <button :class="{ 'is-active': isActive.underline() }" @click="commands.underline">
             Underline
           </button>
           <button @click="showImagePrompt(commands.image)">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="w-4 h-4 fill-current icon-photo"
-            >
-              <path
-                class="primary text-white"
-                d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm9 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"
-              ></path>
-              <path
-                class="secondary text-gray-700"
-                d="M15.3 12.3a1 1 0 0 1 1.4 0l2 2a1 1 0 0 1 .3.7v3a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-3a1 1 0 0 1 .3-.7l4-4a1 1 0 0 1 1.4 0l3.3 3.29 1.3-1.3z"
-              ></path>
+            <svg viewBox="0 0 24 24" class="w-4 h-4 fill-current icon-photo">
+              <path class="primary text-white" d="M5 3h14a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm9 6a2 2 0 1 0 0-4 2 2 0 0 0 0 4z"></path>
+              <path class="secondary text-gray-700" d="M15.3 12.3a1 1 0 0 1 1.4 0l2 2a1 1 0 0 1 .3.7v3a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-3a1 1 0 0 1 .3-.7l4-4a1 1 0 0 1 1.4 0l3.3 3.29 1.3-1.3z"></path>
             </svg>
           </button>
           <button @click="showIframePrompt(commands.iframe)">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 24 24"
-              class="w-4 h-4 fill-current icon-film"
-            >
-              <path
-                class="primary text-white"
-                d="M4 3h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v2h2V5H4zm0 4v2h2V9H4zm0 4v2h2v-2H4zm0 4v2h2v-2H4zM18 5v2h2V5h-2zm0 4v2h2V9h-2zm0 4v2h2v-2h-2zm0 4v2h2v-2h-2z"
-              ></path>
-              <path
-                class="secondary text-gray-700"
-                d="M9 5h6a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1zm0 8h6a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1z"
-              ></path>
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="w-4 h-4 fill-current icon-film">
+              <path class="primary text-white" d="M4 3h16a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V5c0-1.1.9-2 2-2zm0 2v2h2V5H4zm0 4v2h2V9H4zm0 4v2h2v-2H4zm0 4v2h2v-2H4zM18 5v2h2V5h-2zm0 4v2h2V9h-2zm0 4v2h2v-2h-2zm0 4v2h2v-2h-2z"></path>
+              <path class="secondary text-gray-700" d="M9 5h6a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1V6a1 1 0 0 1 1-1zm0 8h6a1 1 0 0 1 1 1v4a1 1 0 0 1-1 1H9a1 1 0 0 1-1-1v-4a1 1 0 0 1 1-1z"></path>
             </svg>
           </button>
-          <button
-            :class="{ 'is-active': isActive.heading({ level: 2 }) }"
-            @click="commands.heading({ level: 2 })"
-          >
+          <button :class="{ 'is-active': isActive.heading({ level: 2 }) }" @click="commands.heading({ level: 2 })">
             H2
           </button>
-          <button
-            :class="{ 'is-active': isActive.heading({ level: 3 }) }"
-            @click="commands.heading({ level: 3 })"
-          >
+          <button :class="{ 'is-active': isActive.heading({ level: 3 }) }" @click="commands.heading({ level: 3 })">
             H3
           </button>
         </div>
@@ -224,58 +153,6 @@
             </template>
           </Question>
         </div>
-        <!-- <ol>
-          <li v-for="(question, index) in questions" :key="index">
-            <div class="flex">
-              <p>{{ question.text }}</p>
-            </div>
-            <ul>
-              <li
-                v-for="answer in question.answers"
-                :key="answer._id"
-                
-              >
-                <svg
-                  v-if="answer.isRight"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  class="w-8 mr-4 fill-current icon-check"
-                >
-                  <circle
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    class="text-green-400"
-                  ></circle>
-                  <path
-                    class="text-green-800"
-                    d="M10 14.59l6.3-6.3a1 1 0 0 1 1.4 1.42l-7 7a1 1 0 0 1-1.4 0l-3-3a1 1 0 0 1 1.4-1.42l2.3 2.3z"
-                  ></path>
-                </svg>
-                <svg
-                  v-else
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 24 24"
-                  class="w-8 mr-4 fill-current icon-close-circle"
-                >
-                  <circle
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    class="text-red-400"
-                  ></circle>
-                  <path
-                    class="text-red-800"
-                    d="M13.41 12l2.83 2.83a1 1 0 0 1-1.41 1.41L12 13.41l-2.83 2.83a1 1 0 1 1-1.41-1.41L10.59 12 7.76 9.17a1 1 0 0 1 1.41-1.41L12 10.59l2.83-2.83a1 1 0 0 1 1.41 1.41L13.41 12z"
-                  ></path>
-                </svg>
-                <p class="inline">{{ answer.text }}</p>
-              </li>
-              <p>{{ question.theMoreYouKnow }}</p>
-            </ul>
-          </li>
-        </ol>
-      </div> -->
     </section>
   </main>
 </template>
@@ -283,7 +160,7 @@
 <script>
 import { mapGetters } from 'vuex'
 import Api from '@/services/Api'
-import { PageHeading, ButtonBase, ButtonPrimary, ButtonSecondary, ButtonDanger } from '@/components/BaseUI/'
+import { PageHeading, ButtonBase, ButtonPrimary, ButtonSecondary, ButtonDanger, BaseInput, } from '@/components/BaseUI/'
 import CreateQuestion from '@/components/quiz/CreateQuestion'
 import Question from '@/components/quiz/Question'
 
@@ -318,6 +195,7 @@ export default {
     ButtonPrimary,
     ButtonSecondary,
     ButtonDanger,
+    BaseInput,
     CreateQuestion,
     Question
   },
@@ -474,9 +352,6 @@ export default {
       } catch (error) {
         console.log(error)
       }
-    },
-    logLesson() {
-      console.log(this.editor.getJSON())
     },
     onNewQuestion(newQuestion) {
       this.questions.push(newQuestion)
